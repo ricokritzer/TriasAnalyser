5c3d89c181431086a2929a8ee35983789c4692dd
ricokritzer <rico@kritzer.net>
2018-10-15 13:36:11 +0200

    Klasse zur Überwachung der Haltestellen angelegt.

---
+++ Studienarbeit/Data/src/main/java/de/dhbw/studienarbeit/StopWatcher.java
@@ -0,0 +1,52 @@
+package de.dhbw.studienarbeit;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StopWatcher
+{
+	private final List<String> stopIDsWithResponse = new ArrayList<>();
+	private final List<String> stopIDsWithoutResponse = new ArrayList<>();
+
+	private int idx = 1;
+	private static final String STOP_ID_PRE = "de:8212:";
+
+	public String createNewStopID()
+	{
+		final String stopID = STOP_ID_PRE + idx;
+		idx++;
+		return stopID;
+	}
+
+	public void addToListWithResponse(String stopID)
+	{
+		stopIDsWithResponse.add(stopID);
+	}
+
+	public void addToListWithoutResponse(String stopID)
+	{
+		stopIDsWithoutResponse.add(stopID);
+	}
+
+	@Deprecated
+	public void printStopIDsWithResponse()
+	{
+		stopIDsWithResponse.forEach(System.out::println);
+	}
+
+	@Deprecated
+	public void printStopIDsWithoutResponse()
+	{
+		stopIDsWithoutResponse.forEach(System.out::println);
+	}
+
+	public int countStopIDsWithResponse()
+	{
+		return stopIDsWithResponse.size();
+	}
+
+	public int countStopIDsWithoutResponse()
+	{
+		return stopIDsWithoutResponse.size();
+	}
+}
