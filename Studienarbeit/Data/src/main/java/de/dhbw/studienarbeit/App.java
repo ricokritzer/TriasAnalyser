59ee009b70d456317fa2bacb6dc1e2d1900bf204
Patrick <pat-sie@web.de>
2018-10-15 12:42:02 +0200

    Anfrage an KVV

--- Studienarbeit/Data/src/main/java/de/dhbw/studienarbeit/App.java
+++ Studienarbeit/Data/src/main/java/de/dhbw/studienarbeit/App.java
@@ -1,20 +1,27 @@
 package de.dhbw.studienarbeit;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.io.StringReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-/**
- * Hello world!
- *
- */
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+
+import com.sun.corba.se.impl.orbutil.graph.Node;
+
 public class App {
 	public static void main(String[] args) {
-		try
-		{
+		try {
 			URL url = new URL("http://185.201.144.208/kritzertrias/trias");
 			URLConnection con = url.openConnection();
 			con.setDoInput(true);
@@ -25,10 +32,11 @@ public class App {
 			con.setDefaultUseCaches(false);
 			con.setRequestProperty("Content-Type", "text/xml");
 			OutputStreamWriter writer = new OutputStreamWriter(con.getOutputStream());
-			
+
 			StringBuilder sb = new StringBuilder();
 			sb.append("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
-			sb.append("<Trias version=\"1.1\" xmlns=\"http://www.vdv.de/trias\" xmlns:siri=\"http://www.siri.org.uk/siri\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">");
+			sb.append(
+					"<Trias version=\"1.1\" xmlns=\"http://www.vdv.de/trias\" xmlns:siri=\"http://www.siri.org.uk/siri\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">");
 			sb.append("<ServiceRequest>");
 			sb.append("<siri:RequestorRef>7qzuxfx8EPvH</siri:RequestorRef>");
 			sb.append("<RequestPayload>");
@@ -37,7 +45,7 @@ public class App {
 			sb.append("<LocationRef>");
 			sb.append("<StopPointRef>de:8212:7</StopPointRef>");
 			sb.append("</LocationRef>");
-			sb.append("<DepArrTime>2018-10-15T9:50:00</DepArrTime>");
+			sb.append("<DepArrTime>" + currentTime() + "</DepArrTime>");
 			sb.append("</Location>");
 			sb.append("<Params>");
 			sb.append("<NumberOfResults>10</NumberOfResults>");
@@ -48,26 +56,41 @@ public class App {
 			sb.append("</RequestPayload>");
 			sb.append("</ServiceRequest>");
 			sb.append("</Trias>");
-			
+
 			writer.write(sb.toString());
 			writer.flush();
 			writer.close();
-			
-			
+
 			// reading the response
 			InputStreamReader reader = new InputStreamReader(con.getInputStream());
 			BufferedReader br = new BufferedReader(reader);
 			StringBuilder sbResponse = new StringBuilder();
-			while (br.ready())
-			{
+			while (br.ready()) {
 				sbResponse.append(br.readLine());
 			}
-			System.out.println(sbResponse.toString());
 			br.close();
-		}
-		catch (Throwable t)
-		{
+
+			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+			DocumentBuilder parser = factory.newDocumentBuilder();
+			Document doc = parser.parse(new InputSource(new StringReader(sbResponse.toString())));
+			Element docElement = doc.getDocumentElement();
+			for (int i = 0; i < docElement.getElementsByTagName("EstimatedTime").getLength(); i++) {
+				System.out.println(docElement.getElementsByTagName("PublishedLineName").item(i).getNodeName() + ": "
+						+ docElement.getElementsByTagName("PublishedLineName").item(i).getTextContent());
+				System.out.println(docElement.getElementsByTagName("DestinationText").item(i).getNodeName() + ": "
+						+ docElement.getElementsByTagName("DestinationText").item(i).getTextContent());
+				System.out.println(docElement.getElementsByTagName("EstimatedTime").item(i).getNodeName() + ": "
+						+ docElement.getElementsByTagName("EstimatedTime").item(i).getTextContent());
+			}
+		} catch (Throwable t) {
 			t.printStackTrace(System.out);
 		}
+	}
+
+	private static String currentTime() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+		String currentDate = sdf.format(new Date());
+		System.out.println(currentDate);
+		return currentDate;
 	}
 }
