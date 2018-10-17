4257b55ed737bec9c587b268a512d50580413829
ricokritzer <rico@kritzer.net>
2018-10-17 13:56:19 +0200

    Schleife um die Update-Funktion (mit Ausgabe)

--- Studienarbeit/DataWeather/src/main/java/de/dhbw/studienarbeit/data/weather/App.java
+++ Studienarbeit/DataWeather/src/main/java/de/dhbw/studienarbeit/data/weather/App.java
@@ -1,9 +1,19 @@
 package de.dhbw.studienarbeit.data.weather;
 
+import java.io.IOException;
+
 public class App
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws InterruptedException, IOException
 	{
-		new Coordinates(49.01, 8.4); // Weather of Karlsruhe, DE
+		final long requestCycle = 10; // in seconds
+		final Coordinates karlsruhe = new Coordinates(49.01, 8.4); // Weather of Karlsruhe, DE
+
+		while (true)
+		{
+			System.out.println(karlsruhe);
+			Thread.sleep(requestCycle * 1000);
+			karlsruhe.updateData();
+		}
 	}
 }
