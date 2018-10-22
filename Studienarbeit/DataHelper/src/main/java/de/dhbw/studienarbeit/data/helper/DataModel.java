28429b73e46cdfef9ef26319a8aaea8984e81090
ricokritzer <rico@kritzer.net>
2018-10-22 10:02:40 +0200

    Methode zum Updaten hinzugefügt

--- Studienarbeit/DataHelper/src/main/java/de/dhbw/studienarbeit/data/helper/DataModel.java
+++ Studienarbeit/DataHelper/src/main/java/de/dhbw/studienarbeit/data/helper/DataModel.java
@@ -1,6 +1,10 @@
 package de.dhbw.studienarbeit.data.helper;
 
+import java.io.IOException;
+
 public interface DataModel
 {
 	String getSQLQuerry();
+
+	void updateData(final int attempts) throws IOException;
 }
