1f25a6fe599943cba688b189e47cba418605e7d1
ricokritzer <rico@kritzer.net>
2018-10-22 10:35:07 +0200

    Refactoring: Verwendung von DataManager

--- Studienarbeit/DataWeather/src/main/java/de/dhbw/studienarbeit/data/weather/App.java
+++ Studienarbeit/DataWeather/src/main/java/de/dhbw/studienarbeit/data/weather/App.java
@@ -2,35 +2,14 @@ package de.dhbw.studienarbeit.data.weather;
 
 import java.io.IOException;
 
-import de.dhbw.studienarbeit.data.helper.Saver;
+import de.dhbw.studienarbeit.data.helper.DataManager;
+import de.dhbw.studienarbeit.data.helper.Updaterate;
 
 public class App
 {
 	public static void main(String[] args) throws IOException
 	{
-		final long requestCycle = 60; // in seconds
 		final Weather karlsruhe = new Weather(49.01, 8.4); // Weather of Karlsruhe, DE
-		final Saver saver = new Saver();
-
-		boolean run = true;
-
-		while (run)
-		{
-			try
-			{
-				saver.save(karlsruhe);
-				Thread.sleep(requestCycle * 1000);
-				karlsruhe.updateData(3);
-			}
-			catch (InterruptedException e)
-			{
-				run = false;
-				saver.logError(e);
-			}
-			catch (IOException e)
-			{
-				saver.logError(e);
-			}
-		}
+		new DataManager(karlsruhe, Updaterate.UPDATE_EVERY_MINUTE);
 	}
 }
