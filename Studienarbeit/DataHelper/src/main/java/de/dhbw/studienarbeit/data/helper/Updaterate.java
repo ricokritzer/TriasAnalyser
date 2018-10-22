d8cb2471ab6cbcbc8db7961c5b85a58ba4e86d93
ricokritzer <rico@kritzer.net>
2018-10-22 11:36:20 +0200

    Refactoring

--- Studienarbeit/DataHelper/src/main/java/de/dhbw/studienarbeit/data/helper/Updaterate.java
+++ Studienarbeit/DataHelper/src/main/java/de/dhbw/studienarbeit/data/helper/Updaterate.java
@@ -5,7 +5,7 @@ import java.util.logging.Logger;
 
 public enum Updaterate
 {
-	UPDATE_EVERY_MINUTE(60 * 100), UPDATE_EVERY_FIVE_MINUTES(5 * 60 * 100);
+	UPDATE_EVERY_MINUTE(minutes(1)), UPDATE_EVERY_FIVE_MINUTES(minutes(5));
 
 	private static final Logger LOGGER = Logger.getLogger(Updaterate.class.getName());
 	private long waitingTime;
@@ -15,6 +15,7 @@ public enum Updaterate
 		this.waitingTime = waitingTime;
 	}
 
+	@Deprecated
 	public void waitTime()
 	{
 		try
@@ -25,5 +26,15 @@ public enum Updaterate
 		{
 			LOGGER.log(Level.WARNING, e.getMessage(), e);
 		}
+	}
+
+	public long getDelay()
+	{
+		return waitingTime;
+	}
+
+	private static int minutes(int min)
+	{
+		return 1000 * 60 * min;
 	}
 }
