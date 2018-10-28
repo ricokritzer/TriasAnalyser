ddd1529dae192234c7b195213720b48901881fa8
Patrick <pat-sie@web.de>
2018-10-28 21:58:16 +0100

    Versuch Fehler zu beheben

---
+++ Studienarbeit/DataTrias/src/main/java/de/dhbw/studienarbeit/data/trias/DataTriasApp.java
@@ -0,0 +1,15 @@
+package de.dhbw.studienarbeit.data.trias;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import de.dhbw.studienarbeit.data.helper.DataManager;
+import de.dhbw.studienarbeit.data.helper.DatabaseSaver;
+
+public class DataTriasApp 
+{
+    public void startDataCollection(List<Station> stations) throws SQLException, ReflectiveOperationException
+    {
+//    	DataManager dataManager = new DataManager(new DatabaseSaver(), 60);
+    }
+}
