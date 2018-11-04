0ccbcc39f7af541b11a076b70ac4083ae57656d3
Patrick <pat-sie@web.de>
2018-11-04 11:57:17 +0100

    nextUpdate für Station

--- Studienarbeit/DataTrias/src/main/java/de/dhbw/studienarbeit/data/trias/Station.java
+++ Studienarbeit/DataTrias/src/main/java/de/dhbw/studienarbeit/data/trias/Station.java
@@ -2,6 +2,7 @@ package de.dhbw.studienarbeit.data.trias;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -71,5 +72,41 @@ public class Station implements DataModel
 		previousStops = currentStops;
 		TriasXMLRequest request = new TriasXMLRequest(apiKey, this);
 		currentStops = request.getResponse();
+		checkNextUpdate();
+	}
+
+	private void checkNextUpdate()
+	{
+		if (nextUpdate == null)
+		{
+			nextUpdate = new Date();
+			return;
+		}
+		
+		Calendar cal = Calendar.getInstance();
+		if (currentStops.isEmpty())
+		{
+			cal.setTime(nextUpdate);
+			cal.add(Calendar.HOUR_OF_DAY, 2);
+			nextUpdate = cal.getTime();
+			return;
+		}
+		
+		Stop nextStop = currentStops.get(0);
+		cal.setTime(nextStop.getRealTime());
+		cal.add(Calendar.MINUTE, -5);
+		
+		Calendar inFiveMinutes = Calendar.getInstance();
+		inFiveMinutes.setTime(new Date());
+		inFiveMinutes.add(Calendar.MINUTE, 5);
+		
+		if (cal.before(inFiveMinutes))
+		{
+			cal.add(Calendar.MINUTE, 4);
+			nextUpdate = cal.getTime();
+			return;
+		}
+		
+		nextUpdate = cal.getTime();
 	}
 }
