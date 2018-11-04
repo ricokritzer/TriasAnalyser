28ea48da676e41908d54e0ee65005424bee12451
ricokritzer <rico@kritzer.net>
2018-11-04 15:25:16 +0100

    Refactoring

--- Studienarbeit/DataWeather/src/main/java/de/dhbw/studienarbeit/data/weather/DataWeatherApp.java
+++ Studienarbeit/DataWeather/src/main/java/de/dhbw/studienarbeit/data/weather/DataWeatherApp.java
@@ -3,8 +3,6 @@ package de.dhbw.studienarbeit.data.weather;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import de.dhbw.studienarbeit.data.helper.Settings;
 import de.dhbw.studienarbeit.data.helper.database.model.StationDB;
@@ -14,9 +12,7 @@ import de.dhbw.studienarbeit.data.helper.datamanagement.DataManager;
 
 public class DataWeatherApp
 {
-	private static final Logger LOGGER = Logger.getLogger(DataWeatherApp.class.getName());
-
-	public static void main(String[] args)
+	public static void main(String[] args) throws IOException
 	{
 		List<StationDB> testStations = new ArrayList<>();
 		testStations.add(new StationDB("de:test:Karlsruhe", "Karlsruhe", 49.01, 8.40, "Stadt", true));
@@ -24,28 +20,17 @@ public class DataWeatherApp
 		new DataWeatherApp().startDataCollection(testStations);
 	}
 
-	public void startDataCollection(final List<StationDB> stations)
+	public void startDataCollection(final List<StationDB> stations) throws IOException
 	{
-		try
-		{
-			final List<ApiKey> apiKeys = Settings.getInstance().getApiKeys("weather");
-			final DataManager manager = new DataManager(new DatabaseSaver(), apiKeys);
-			final List<Weather> weather = convertToWeather(stations);
-
-			manager.add(weather);
-
-			LOGGER.log(Level.INFO, stations.size() + " stations converted into " + weather.size() + " Weather.");
-		}
-		catch (IOException e)
-		{
-			LOGGER.log(Level.WARNING, "Unable to start data collection.", e);
-		}
+		final List<ApiKey> apiKeys = Settings.getInstance().getApiKeys("weather");
+		final DataManager manager = new DataManager(new DatabaseSaver(), apiKeys);
+		final List<Weather> weather = convertToWeather(stations);
+		manager.add(weather);
 	}
 
 	protected List<Weather> convertToWeather(List<StationDB> stations)
 	{
 		final List<Weather> weather = new ArrayList<>();
-
 		stations.forEach(s -> {
 			final Weather w = convertToWeather(s);
 			if (!weather.contains(w))
@@ -53,7 +38,6 @@ public class DataWeatherApp
 				weather.add(w);
 			}
 		});
-
 		return weather;
 	}
 
