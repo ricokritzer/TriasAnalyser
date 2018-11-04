60bdd2486b7375e210518d0d99ffb633f6943c48
ricokritzer <rico@kritzer.net>
2018-11-04 16:32:54 +0100

    Refactoring

--- Studienarbeit/DataHelper/src/main/java/de/dhbw/studienarbeit/data/helper/database/table/DatabaseTable.java
+++ Studienarbeit/DataHelper/src/main/java/de/dhbw/studienarbeit/data/helper/database/table/DatabaseTable.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -19,12 +20,12 @@ public abstract class DatabaseTable extends DatabaseConnector
 	private static final String UNABLE_TO_READ = "Unable to read at table ";
 	private static final String START_READING_AT_TABLE = "Start reading at table ";
 	private static final String ENTRIES_READ = " entries read.";
-	private static final String SELECT_FROM = "*";
+	private static final String ALL = "*";
 	private static final Logger LOGGER = Logger.getLogger(DatabaseTable.class.getName());
 
 	protected String createSQLStatement(final String tableName, final SqlCondition... condition)
 	{
-		return createSQLStatement(SELECT_FROM, tableName, condition);
+		return createSQLStatement(ALL, tableName, condition);
 	}
 
 	private String createSQLStatement(final String what, final String tableName, final SqlCondition... condition)
@@ -44,6 +45,12 @@ public abstract class DatabaseTable extends DatabaseConnector
 	}
 
 	protected void select(Consumer<ResultSet> consumer, String tableName, SqlCondition... conditions)
+			throws SQLException
+	{
+		select(consumer, ALL, tableName, conditions);
+	}
+
+	private void select(Consumer<ResultSet> consumer, String what, String tableName, SqlCondition... conditions)
 			throws SQLException
 	{
 		LOGGER.log(Level.INFO, START_READING_AT_TABLE + tableName);
@@ -79,33 +86,28 @@ public abstract class DatabaseTable extends DatabaseConnector
 				Settings.getInstance().getDatabaseReaderPassword());
 	}
 
-	protected int count(final String tableName, final SqlCondition... conditions) throws SQLException
+	private Optional<Integer> getTotal(ResultSet result)
 	{
-		final String what = "COUNT(*) AS total";
-		int count = 0;
-		LOGGER.log(Level.INFO, START_READING_AT_TABLE + tableName);
-
-		reconnectIfNeccessary();
-
-		final String sql = createSQLStatement(what, tableName, conditions);
-		try (ResultSet result = connection.prepareStatement(sql).executeQuery())
+		try
 		{
-			while (result.next())
-			{
-				count = result.getInt("total");
-			}
+			return Optional.ofNullable(result.getInt("total"));
 		}
 		catch (SQLException e)
 		{
-			LOGGER.log(Level.WARNING, UNABLE_TO_READ + tableName, e);
-			throw e;
+			return Optional.empty();
 		}
-		finally
-		{
-			disconnect();
-		}
+	}
 
-		return count;
+	protected int count(final String tableName, final SqlCondition... conditions) throws SQLException
+	{
+		final String what = "COUNT(*) AS total";
+		List<Integer> count = new ArrayList<>();
+		select(result -> getTotal(result).ifPresent(count::add), what, tableName, conditions);
+		if (count.isEmpty())
+		{
+			throw new SQLException("Unable to count entries in " + tableName);
+		}
+		return count.get(0);
 	}
 
 	public abstract int count(SqlCondition... conditions) throws IOException;
