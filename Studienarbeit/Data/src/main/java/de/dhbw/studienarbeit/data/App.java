1f2ee2eae87b301b7d51c3e990171021dacf12f9
ricokritzer <rico@kritzer.net>
2018-11-04 15:14:19 +0100

    Deprecated Methoden ausgebaut

--- Studienarbeit/Data/src/main/java/de/dhbw/studienarbeit/data/App.java
+++ Studienarbeit/Data/src/main/java/de/dhbw/studienarbeit/data/App.java
@@ -1,22 +1,20 @@
 package de.dhbw.studienarbeit.data;
 
 import java.io.IOException;
-import java.sql.SQLException;
 import java.util.List;
 
-import de.dhbw.studienarbeit.data.helper.database.DatabaseReader;
 import de.dhbw.studienarbeit.data.helper.database.model.StationDB;
 import de.dhbw.studienarbeit.data.helper.database.saver.DatabaseSaver;
+import de.dhbw.studienarbeit.data.helper.database.table.DatabaseTableStation;
 import de.dhbw.studienarbeit.data.repairer.DataRepairerApp;
 import de.dhbw.studienarbeit.data.trias.DataTriasApp;
 import de.dhbw.studienarbeit.data.weather.DataWeatherApp;
 
 public class App
 {
-	public static void main(String[] args) throws IOException, SQLException
+	public static void main(String[] args) throws IOException
 	{
-		final DatabaseReader reader = new DatabaseReader();
-		final List<StationDB> stations = reader.readStations();
+		final List<StationDB> stations = new DatabaseTableStation().selectObservedStations();
 		new DataWeatherApp().startDataCollection(stations);
 		new DataTriasApp().startDataCollection(stations);
 		new DataRepairerApp(new DatabaseSaver()).startDataRepairing();
