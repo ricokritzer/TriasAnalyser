c9e18144a0890e494280e001e09fccaf9ab82fb1
ricokritzer <rico@kritzer.net>
2018-11-05 13:04:51 +0100

    Test für neuen DataManager eingefügt

---
+++ Studienarbeit/DataHelper/src/test/java/de/dhbw/studienarbeit/data/helper/datamanagement/DataManager2Test.java
@@ -0,0 +1,47 @@
+package de.dhbw.studienarbeit.data.helper.datamanagement;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+public class DataManager2Test
+{
+	int updates = 0;
+	Date nextUpdate = new Date();
+	boolean saved = false;
+
+	final DataModel2 data = new DataModel2()
+	{
+		@Override
+		public Date nextUpdate()
+		{
+			return nextUpdate;
+		}
+
+		@Override
+		public void updateAndSaveData(ApiKey apiKey) throws IOException
+		{
+			updates++;
+		}
+	};
+
+	@Test
+	void testCountUpdateIsLessThanMaximum() throws Exception
+	{
+		final List<ApiKey> apiKeys = new ArrayList<>();
+		apiKeys.add(new ApiKey("foo", 60, "bar"));
+
+		final DataManager2 manager = new DataManager2(apiKeys);
+		manager.add(data);
+
+		Thread.sleep(6000);
+
+		assertTrue(updates <= 7);
+		assertTrue(updates > 5);
+	}
+}
