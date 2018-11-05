13c05b21d3b08358dc8e7aa0fc5f9ad4f82cda43
ricokritzer <rico@kritzer.net>
2018-11-05 10:14:56 +0100

    Verkehrsbetriebe können jetzt aus DB gelesen werden.

--- Studienarbeit/DataHelper/src/main/java/de/dhbw/studienarbeit/data/helper/database/table/DatabaseTableStation.java
+++ Studienarbeit/DataHelper/src/main/java/de/dhbw/studienarbeit/data/helper/database/table/DatabaseTableStation.java
@@ -4,8 +4,10 @@ import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -36,6 +38,20 @@ public class DatabaseTableStation extends DatabaseTable
 		}
 	}
 
+	private final Optional<String> getOperator(ResultSet result)
+	{
+		try
+		{
+			final String operator = result.getString("operator");
+			return Optional.of(operator);
+		}
+		catch (SQLException e)
+		{
+			LOGGER.log(Level.WARNING, "Unable to parse to Operator.", e);
+			return Optional.empty();
+		}
+	}
+
 	@Override
 	protected String getTableName()
 	{
@@ -49,6 +65,20 @@ public class DatabaseTableStation extends DatabaseTable
 			final List<StationDB> stations = new ArrayList<>();
 			select(r -> getStation(r).ifPresent(stations::add), TABLE_NAME, conditions);
 			return stations;
+		}
+		catch (SQLException e)
+		{
+			throw new IOException("Selecting does not succeed.", e);
+		}
+	}
+
+	public final List<String> selectOperators() throws IOException
+	{
+		try
+		{
+			final Set<String> operators = new HashSet<>();
+			select(r -> getOperator(r).ifPresent(operators::add), TABLE_NAME);
+			return new ArrayList<>(operators);
 		}
 		catch (SQLException e)
 		{
--- Studienarbeit/DataHelper/src/test/java/de/dhbw/studienarbeit/data/helper/database/table/DatabaseTableStationTest.java
+++ Studienarbeit/DataHelper/src/test/java/de/dhbw/studienarbeit/data/helper/database/table/DatabaseTableStationTest.java
@@ -1,8 +1,10 @@
 package de.dhbw.studienarbeit.data.helper.database.table;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
@@ -14,6 +16,20 @@ public class DatabaseTableStationTest
 		try
 		{
 			new DatabaseTableStation().count();
+		}
+		catch (IOException e)
+		{
+			fail("Unable to count stations" + e.getMessage());
+		}
+	}
+
+	@Test
+	void testSelectingOperators() throws Exception
+	{
+		try
+		{
+			List<String> operators = new DatabaseTableStation().selectOperators();
+			assertTrue(operators.size() > 0);
 		}
 		catch (IOException e)
 		{
