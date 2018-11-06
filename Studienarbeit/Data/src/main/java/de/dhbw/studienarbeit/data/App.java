9581f9cec6fc04b6242b72b5f3fd2c35c49c4104
ricokritzer <rico@kritzer.net>
2018-11-06 09:06:36 +0100

    Loglevel konfigurierbar

--- Studienarbeit/Data/src/main/java/de/dhbw/studienarbeit/data/App.java
+++ Studienarbeit/Data/src/main/java/de/dhbw/studienarbeit/data/App.java
@@ -1,7 +1,11 @@
 package de.dhbw.studienarbeit.data;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 import de.dhbw.studienarbeit.data.helper.database.model.StationDB;
 import de.dhbw.studienarbeit.data.helper.database.saver.DatabaseSaver;
@@ -14,6 +18,8 @@ public class App
 {
 	public static void main(String[] args) throws IOException
 	{
+		setLogLevel(Level.INFO);
+
 		final DatabaseTableStation databaseTableStation = new DatabaseTableStation();
 		final List<String> operators = databaseTableStation.selectObservedOperators();
 		for (String operator : operators)
@@ -24,5 +30,12 @@ public class App
 		}
 
 		new DataRepairerApp(new DatabaseSaver()).startDataRepairing();
+	}
+
+	private static void setLogLevel(Level level)
+	{
+		final Logger rootLogger = LogManager.getLogManager().getLogger("");
+		rootLogger.setLevel(level);
+		Arrays.asList(rootLogger.getHandlers()).forEach(h -> h.setLevel(level));
 	}
 }
