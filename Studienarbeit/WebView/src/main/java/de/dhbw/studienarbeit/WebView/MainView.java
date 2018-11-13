719cdce2e0a927ebc7692543c35f471ceb75966a
ricokritzer <rico@kritzer.net>
2018-11-13 19:00:41 +0100

    Heatmap-Grundlagen angelegt

--- Studienarbeit/WebView/src/main/java/de/dhbw/studienarbeit/WebView/MainView.java
+++ Studienarbeit/WebView/src/main/java/de/dhbw/studienarbeit/WebView/MainView.java
@@ -13,14 +13,16 @@ import com.vaadin.flow.router.Route;
 import de.dhbw.studienarbeit.WebView.components.AboutDiv;
 import de.dhbw.studienarbeit.WebView.components.DatabaseDiv;
 import de.dhbw.studienarbeit.WebView.components.DelayDiv;
+import de.dhbw.studienarbeit.WebView.components.HeatmapDiv;
 
 /**
  * The main view contains a button and a click listener.
  */
-@SuppressWarnings("serial")
 @Route("")
 public class MainView extends VerticalLayout
 {
+	private static final long serialVersionUID = 4L;
+
 	public MainView()
 	{
 		setWidth("100%");
@@ -28,22 +30,26 @@ public class MainView extends VerticalLayout
 
 		Tab tabDatabase = new Tab("Unsere Daten");
 		Tab tabDelay = new Tab("Verspätungen");
+		Tab tabHeatmap = new Tab("Heatmap");
 		Tab tabAbout = new Tab("Über uns");
 
-		Tabs tabs = new Tabs(tabDatabase, tabDelay, tabAbout);
+		Tabs tabs = new Tabs(tabDatabase, tabDelay, tabHeatmap, tabAbout);
 
 		Div divDatabase = new DatabaseDiv();
 		Div divDelay = new DelayDiv();
 		Div divAbout = new AboutDiv();
+		Div divHeatmap = new HeatmapDiv();
 
 		Map<Tab, Component> tabsToPages = new HashMap<>();
 		tabsToPages.put(tabDatabase, divDatabase);
 		tabsToPages.put(tabDelay, divDelay);
 		tabsToPages.put(tabAbout, divAbout);
+		tabsToPages.put(tabHeatmap, divHeatmap);
 
 		add(tabs);
 		add(divDatabase);
 		add(divDelay);
+		add(divHeatmap);
 		add(divAbout);
 
 		divDatabase.setVisible(true);
