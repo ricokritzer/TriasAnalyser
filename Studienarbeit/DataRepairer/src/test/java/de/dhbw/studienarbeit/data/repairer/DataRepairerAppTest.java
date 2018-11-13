d567d9117defeb378624a69678437261b1c10589
ricokritzer <rico@kritzer.net>
2018-11-13 15:39:29 +0100

    Fehlerbehebung. Funktionales Interface

--- Studienarbeit/DataRepairer/src/test/java/de/dhbw/studienarbeit/data/repairer/DataRepairerAppTest.java
+++ Studienarbeit/DataRepairer/src/test/java/de/dhbw/studienarbeit/data/repairer/DataRepairerAppTest.java
@@ -4,9 +4,14 @@ import static org.junit.Assert.assertThat;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
+import java.io.IOException;
 
 import org.hamcrest.core.Is;
 import org.junit.jupiter.api.Test;
+
+import de.dhbw.studienarbeit.data.helper.database.saver.Saveable;
+import de.dhbw.studienarbeit.data.helper.database.saver.Saveable2;
+import de.dhbw.studienarbeit.data.helper.database.saver.Saver;
 
 public class DataRepairerAppTest
 {
@@ -15,7 +20,20 @@ public class DataRepairerAppTest
 	@Test
 	void testRepairing() throws Exception
 	{
-		final DataRepairerApp app = new DataRepairerApp(model -> savedModelSQL = model.getSQLQuerry());
+		final DataRepairerApp app = new DataRepairerApp(new Saver()
+		{
+			@Override
+			public void save(Saveable2 model) throws IOException
+			{
+				savedModelSQL = model.toString();
+			}
+
+			@Override
+			public void save(Saveable model) throws IOException
+			{
+				savedModelSQL = model.getSQLQuerry();
+			}
+		});
 
 		try (BufferedWriter bw = new BufferedWriter(new FileWriter("errors.txt")))
 		{
