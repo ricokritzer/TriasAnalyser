54ca09c4b7a87b951ad07cae2bf2107e459f5706
ricokritzer <rico@kritzer.net>
2018-11-13 19:06:14 +0100

    Fehlerhaften Code als Kommentar

--- Studienarbeit/WebView/src/main/java/de/dhbw/studienarbeit/WebView/components/HeatmapDiv.java
+++ Studienarbeit/WebView/src/main/java/de/dhbw/studienarbeit/WebView/components/HeatmapDiv.java
@@ -1,12 +1,5 @@
 package de.dhbw.studienarbeit.WebView.components;
 
-import org.vaadin.addon.leaflet.LMap;
-import org.vaadin.addon.leaflet.LMarker;
-import org.vaadin.addon.leaflet.LeafletLayer;
-import org.vaadin.addon.leaflet.shared.Point;
-import org.vaadin.addon.leaflet.shramov.LGoogleLayer;
-import org.vaadin.addon.leaflet.shramov.LGoogleLayer.Type;
-
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -21,23 +14,23 @@ public class HeatmapDiv extends Div
 
 		layout.add(new Text("Folgt in Kürze ..."));
 
-		final LMap leafletMap = new LMap();
-		leafletMap.setCenter(60.4525, 22.301);
-		leafletMap.setZoomLevel(15.0);
-
-		final LMarker m = new LMarker(60.4525, 22.301);
-		m.setDivIcon("Hello <strong>world</strong>!");
-		m.setIconSize(new Point(80, 20));
-
-		final LMarker leafletMarker = new LMarker(60.4525, 22.301);
-		leafletMarker.setIconSize(new Point(57, 52));
-		leafletMarker.setIconAnchor(new Point(57, 26));
-		leafletMarker.setTitle("this is marker one!");
-		leafletMarker.setPopup("Hello <b>Vaadin World</b>!");
-
-		final LeafletLayer l = new LGoogleLayer(Type.SATELLITE);
-
-		leafletMap.addBaseLayer(l, "Google Maps layer");
+		// final LMap leafletMap = new LMap();
+		// leafletMap.setCenter(60.4525, 22.301);
+		// leafletMap.setZoomLevel(15.0);
+		//
+		// final LMarker m = new LMarker(60.4525, 22.301);
+		// m.setDivIcon("Hello <strong>world</strong>!");
+		// m.setIconSize(new Point(80, 20));
+		//
+		// final LMarker leafletMarker = new LMarker(60.4525, 22.301);
+		// leafletMarker.setIconSize(new Point(57, 52));
+		// leafletMarker.setIconAnchor(new Point(57, 26));
+		// leafletMarker.setTitle("this is marker one!");
+		// leafletMarker.setPopup("Hello <b>Vaadin World</b>!");
+		//
+		// final LeafletLayer l = new LGoogleLayer(Type.SATELLITE);
+		//
+		// leafletMap.addBaseLayer(l, "Google Maps layer");
 
 		// layout.add(leafletMap);
 
