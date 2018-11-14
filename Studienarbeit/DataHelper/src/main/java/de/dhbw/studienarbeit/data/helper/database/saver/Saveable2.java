68aee3163fc719109275ed967b1572f341fb624c
ricokritzer <rico@kritzer.net>
2018-11-14 08:43:24 +0100

    Deprecated gesetzt -> zu fehleranfällig.

--- Studienarbeit/DataHelper/src/main/java/de/dhbw/studienarbeit/data/helper/database/saver/Saveable2.java
+++ Studienarbeit/DataHelper/src/main/java/de/dhbw/studienarbeit/data/helper/database/saver/Saveable2.java
@@ -4,7 +4,9 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
+@Deprecated
 public interface Saveable2
 {
+	@Deprecated
 	PreparedStatement getPreparedStatement(Connection connection) throws SQLException;
 }
