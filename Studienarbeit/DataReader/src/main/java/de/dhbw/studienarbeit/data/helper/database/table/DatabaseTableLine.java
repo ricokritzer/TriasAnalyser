f91b0d17453c937c6a0055d7c7896c9b67fb7896
ricokritzer <rico@kritzer.net>
2018-11-16 14:08:52 +0100

    Cache

--- Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/helper/database/table/DatabaseTableLine.java
+++ Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/helper/database/table/DatabaseTableLine.java
@@ -5,7 +5,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -16,6 +18,7 @@ public class DatabaseTableLine extends DatabaseTable
 {
 	private static final Logger LOGGER = Logger.getLogger(DatabaseTableLine.class.getName());
 	private static final String TABLE_NAME = "Line";
+	private static final Map<String, Map<String, Integer>> CACHE = new HashMap<>();
 
 	private final Optional<LineDB> getLine(ResultSet result)
 	{
@@ -39,6 +42,10 @@ public class DatabaseTableLine extends DatabaseTable
 		return TABLE_NAME;
 	}
 
+	/*
+	 * @deprecated: use cached method getLineID instead.
+	 */
+	@Deprecated
 	public List<LineDB> selectLinesByNameAndDestination(String name, String destination) throws IOException
 	{
 		reconnectIfNeccessary();
@@ -57,5 +64,70 @@ public class DatabaseTableLine extends DatabaseTable
 		{
 			throw new IOException("Selecting does not succeed.", e);
 		}
+	}
+
+	public Optional<Integer> getLineID(String lineName, String destination) throws IOException
+	{
+		Optional<Integer> lineIDFromCache = getLineIDFromCache(lineName, destination);
+		if (lineIDFromCache.isPresent())
+		{
+			return lineIDFromCache;
+		}
+
+		return getLineIDFromDatabase(lineName, destination);
+	}
+
+	private Optional<Integer> getLineIDFromDatabase(String lineName, String destination) throws IOException
+	{
+		reconnectIfNeccessary();
+
+		final String sql = "SELECT * FROM " + TABLE_NAME + " WHERE name = ? AND destination = ?;";
+		try (PreparedStatement preparedStatement = connection.prepareStatement(sql))
+		{
+			preparedStatement.setString(1, lineName);
+			preparedStatement.setString(2, destination);
+
+			final List<LineDB> lines = new ArrayList<>();
+			select(r -> getLine(r).ifPresent(lines::add), preparedStatement);
+
+			if (lines.isEmpty())
+			{
+				return Optional.empty();
+			}
+
+			final LineDB line = lines.get(0);
+			cache(line);
+
+			return Optional.ofNullable(lines.get(0).getLineID());
+		}
+		catch (SQLException e)
+		{
+			throw new IOException("Selecting does not succeed.", e);
+		}
+	}
+
+	private void cache(LineDB line)
+	{
+		final Optional<Map<String, Integer>> cachedDestinations = Optional.ofNullable(CACHE.get(line.getName()));
+		final Map<String, Integer> destinations = cachedDestinations.orElse(new HashMap<>());
+		destinations.put(line.getDestination(), line.getLineID());
+		CACHE.put(line.getName(), destinations);
+	}
+
+	protected Optional<Integer> getLineIDFromCache(String lineName, String destination)
+	{
+		final Optional<Map<String, Integer>> destinations = getDirectionsOf(lineName);
+		if (!destinations.isPresent())
+		{
+			return Optional.empty();
+		}
+
+		final Map<String, Integer> dir = destinations.get();
+		return Optional.ofNullable(dir.get(destination));
+	}
+
+	private Optional<Map<String, Integer>> getDirectionsOf(String lineName)
+	{
+		return Optional.ofNullable(CACHE.get(lineName));
 	}
 }
--- Studienarbeit/DataReader/src/test/java/de/dhbw/studienarbeit/data/helper/database/table/DatabaseTableLineTest.java
+++ Studienarbeit/DataReader/src/test/java/de/dhbw/studienarbeit/data/helper/database/table/DatabaseTableLineTest.java
@@ -1,10 +1,13 @@
 package de.dhbw.studienarbeit.data.helper.database.table;
 
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
+import java.util.Optional;
 
+import org.hamcrest.core.Is;
 import org.junit.jupiter.api.Test;
 
 public class DatabaseTableLineTest
@@ -20,6 +23,26 @@ public class DatabaseTableLineTest
 		catch (IOException e)
 		{
 			fail("Unable to count stations" + e.getMessage());
+		}
+	}
+
+	@Test
+	void lineIDFromCache() throws Exception
+	{
+		try
+		{
+			final Optional<Integer> lineID = new DatabaseTableLine().getLineID("R-Bahn RE6", "Neustadt, Hauptbahnhof");
+			assertTrue(lineID.isPresent());
+			assertThat(lineID.get(), Is.is(1));
+
+			final Optional<Integer> lineIDCache = new DatabaseTableLine().getLineIDFromCache("R-Bahn RE6",
+					"Neustadt, Hauptbahnhof");
+			assertTrue(lineIDCache.isPresent());
+			assertThat(lineIDCache.get(), Is.is(1));
+		}
+		catch (IOException e)
+		{
+			fail("Unable to get line ID" + e.getMessage());
 		}
 	}
 }
