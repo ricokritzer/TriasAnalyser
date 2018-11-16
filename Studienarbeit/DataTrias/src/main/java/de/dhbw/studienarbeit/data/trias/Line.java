8c373a80753da76e474d39fb63a135ecb81022de
ricokritzer <rico@kritzer.net>
2018-11-16 21:54:50 +0100

    Refactoring: alte Methoden gelöscht.

--- Studienarbeit/DataTrias/src/main/java/de/dhbw/studienarbeit/data/trias/Line.java
+++ Studienarbeit/DataTrias/src/main/java/de/dhbw/studienarbeit/data/trias/Line.java
@@ -2,33 +2,18 @@ package de.dhbw.studienarbeit.data.trias;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.Objects;
 
 import de.dhbw.studienarbeit.data.helper.database.saver.Saveable;
 
 public class Line implements Saveable
 {
-	private int id;
 	private String name;
 	private String destination;
 
-	public Line(int id, String name, String destination)
-	{
-		this.id = id;
-		this.name = name;
-		this.destination = destination;
-	}
-
 	public Line(String publishedLineName, String destinationText)
 	{
-		this.id = 0;
 		this.name = publishedLineName;
 		this.destination = destinationText;
-	}
-
-	public int getId()
-	{
-		return id;
 	}
 
 	public String getName()
@@ -39,21 +24,6 @@ public class Line implements Saveable
 	public String getDestination()
 	{
 		return destination;
-	}
-
-	/**
-	 * equals evaluates to true, if the id of two lines is the same
-	 */
-	@Override
-	public boolean equals(Object obj)
-	{
-		return obj instanceof Line && ((Line) obj).getId() == id;
-	}
-
-	@Override
-	public int hashCode()
-	{
-		return Objects.hash(id);
 	}
 
 	@Override
