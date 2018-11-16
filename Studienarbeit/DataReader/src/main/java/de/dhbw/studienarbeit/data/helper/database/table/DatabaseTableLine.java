c1d2b76dc6b45b91fab5f91af2cccb78ba7eebad
ricokritzer <rico@kritzer.net>
2018-11-16 21:56:18 +0100

    Refactoring: nicht benutzte Methoden entfernt

--- Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/helper/database/table/DatabaseTableLine.java
+++ Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/helper/database/table/DatabaseTableLine.java
@@ -1,13 +1,7 @@
 package de.dhbw.studienarbeit.data.helper.database.table;
 
-import java.io.IOException;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -18,7 +12,6 @@ public class DatabaseTableLine extends DatabaseTable
 {
 	private static final Logger LOGGER = Logger.getLogger(DatabaseTableLine.class.getName());
 	private static final String TABLE_NAME = "Line";
-	private static final Map<LineCache, Integer> CACHE = new HashMap<>();
 
 	private final Optional<LineDB> getLine(ResultSet result)
 	{
@@ -40,57 +33,5 @@ public class DatabaseTableLine extends DatabaseTable
 	protected String getTableName()
 	{
 		return TABLE_NAME;
-	}
-
-	public Optional<Integer> getLineID(String lineName, String destination) throws IOException
-	{
-		Optional<Integer> lineIDFromCache = getLineIDFromCache(lineName, destination);
-		if (lineIDFromCache.isPresent())
-		{
-			return lineIDFromCache;
-		}
-
-		return getLineIDFromDatabase(lineName, destination);
-	}
-
-	private Optional<Integer> getLineIDFromDatabase(String lineName, String destination) throws IOException
-	{
-		reconnectIfNeccessary();
-
-		final String sql = "SELECT * FROM " + TABLE_NAME + " WHERE name = ? AND destination = ?;";
-		try (PreparedStatement preparedStatement = connection.prepareStatement(sql))
-		{
-			preparedStatement.setString(1, lineName);
-			preparedStatement.setString(2, destination);
-
-			final List<LineDB> lines = new ArrayList<>();
-			select(r -> getLine(r).ifPresent(lines::add), preparedStatement);
-
-			if (lines.isEmpty())
-			{
-				return Optional.empty();
-			}
-
-			final LineDB line = lines.get(0);
-			cache(line);
-
-			return Optional.ofNullable(lines.get(0).getLineID());
-		}
-		catch (SQLException e)
-		{
-			throw new IOException("Selecting does not succeed.", e);
-		}
-	}
-
-	private void cache(LineDB line)
-	{
-		final LineCache lineCache = new LineCache(line.getName(), line.getDestination());
-		CACHE.put(lineCache, line.getLineID());
-	}
-
-	protected Optional<Integer> getLineIDFromCache(String lineName, String destination)
-	{
-		final LineCache line = new LineCache(lineName, destination);
-		return Optional.ofNullable(CACHE.get(line));
 	}
 }
