341f16f8d325d6891bab653d50075d8d9f48a893
ricokritzer <rico@kritzer.net>
2019-01-20 11:12:19 +0100

    Linie in StationNeighbour aufgenommen.

--- Studienarbeit/DataTrias/src/main/java/de/dhbw/studienarbeit/data/trias/StationNeighbour.java
+++ Studienarbeit/DataTrias/src/main/java/de/dhbw/studienarbeit/data/trias/StationNeighbour.java
@@ -9,17 +9,19 @@ public class StationNeighbour implements Saveable
 {
 	private String stationId1;
 	private String stationId2;
+	private int lineID;
 
-	public StationNeighbour(String stationId1, String stationId2)
+	public StationNeighbour(String stationId1, String stationId2, int lineId)
 	{
 		this.stationId1 = stationId1;
 		this.stationId2 = stationId2;
+		this.lineID = lineId;
 	}
 
 	@Override
 	public String getSQLQuerry()
 	{
-		return "INSERT INTO StationNeighbour SELECT * FROM (SELECT ? s1, ? s2) AS tmp WHERE s1 <> s2 AND NOT EXISTS (SELECT stationID1, stationID2 FROM StationNeighbour WHERE stationID1 = s1 AND stationID2 = s2);";
+		return "INSERT INTO StationNeighbour SELECT * FROM (SELECT ? s1, ? s2, ? lineID) AS tmp WHERE s1 <> s2 AND NOT EXISTS (SELECT stationID1, stationID2 FROM StationNeighbour WHERE stationID1 = s1 AND stationID2 = s2);";
 	}
 
 	@Override
@@ -27,5 +29,6 @@ public class StationNeighbour implements Saveable
 	{
 		preparedStatement.setString(1, stationId1);
 		preparedStatement.setString(2, stationId2);
+		preparedStatement.setInt(3, lineID);
 	}
 }
