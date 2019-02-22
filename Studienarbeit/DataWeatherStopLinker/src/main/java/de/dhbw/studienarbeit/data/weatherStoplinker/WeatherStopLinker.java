a4f026f7b1756a08e12b33845fd1622b30c96a8b
ricokritzer <rico@kritzer.net>
2019-02-22 07:56:11 +0100

    Fehlerbehebung (Linker): Bedingung in SQL-Statement fehlerhaft

--- Studienarbeit/DataWeatherStopLinker/src/main/java/de/dhbw/studienarbeit/data/weatherStoplinker/WeatherStopLinker.java
+++ Studienarbeit/DataWeatherStopLinker/src/main/java/de/dhbw/studienarbeit/data/weatherStoplinker/WeatherStopLinker.java
@@ -42,7 +42,7 @@ public class WeatherStopLinker implements Saveable
 				+ "WHERE Stop.stationID = Station.stationID AND Stop.stopID = ? AND Stop.stopID NOT IN (SELECT stopID FROM StopWeather) "
 				+ "AND Weather.lat = ROUND(Station.lat, 2) AND Weather.lon = ROUND(Station.lon, 2) AND Weather.timeStamp < Stop.realTime "
 				+ "GROUP BY Stop.stopID, Stop.realTime, Weather.timeStamp, Weather.id "
-				+ "HAVING count(Weather.timeStamp) > 0 AND max(Weather.timeStamp) > DATE_SUB(Stop.realTime,INTERVAL 30 MINUTE) AND Weather.timeStamp = max(Weather.timeStamp);";
+				+ "HAVING count(Weather.timeStamp) > 0 AND max(Weather.timeStamp) > DATE_SUB(Stop.realTime,INTERVAL 30 MINUTE);";
 	}
 
 	@Override
