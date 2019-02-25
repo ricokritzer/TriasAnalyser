7064884106183a381cf8be3b0162dfc71f02e9d3
ricokritzer <rico@kritzer.net>
2019-02-25 01:31:00 +0100

    Verknüpfung von Stop und Weather optimiert.

--- Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/reader/database/DelayWeatherDBHelper.java
+++ Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/reader/database/DelayWeatherDBHelper.java
@@ -10,11 +10,8 @@ public class DelayWeatherDBHelper
 		return new StringBuilder().append("SELECT ")
 				.append("avg(UNIX_TIMESTAMP(Stop.realTime) - UNIX_TIMESTAMP(Stop.timeTabledTime)) AS delay_avg, ")
 				.append("max(UNIX_TIMESTAMP(Stop.realTime) - UNIX_TIMESTAMP(Stop.timeTabledTime)) AS delay_max, ")
-				.append(what).append(" AS ").append(name).append(" FROM StopWeather, Stop, Weather, Station ")
-				.append("WHERE Stop.stopID = StopWeather.stopID AND Stop.stationID = Station.stationID ")
-				.append("AND Weather.lat = ROUND(Station.lat, 2) AND Weather.lon = ROUND(Station.lon, 2) ")
-				.append(" AND Weather.timeStamp = StopWeather.timeStamp ")
-
+				.append(what).append(" AS ").append(name).append(" FROM StopWeather, Stop, Weather ")
+				.append("WHERE Stop.stopID = StopWeather.stopID AND StopWeather.weatherId = Weather.id ")
 				.append("GROUP BY ").append(name).append(" ORDER BY delay_avg DESC;").toString();
 	}
 }
