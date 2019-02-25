e369e40395b075ef4d5d08c5f1fc32e1bce9524e
ricokritzer <rico@kritzer.net>
2019-02-25 01:37:21 +0100

    Performanceoptimierung: Korrelationskoeffizienten über StopWeather

--- Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/reader/database/DelayWeatherCorrelationHelper.java
+++ Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/reader/database/DelayWeatherCorrelationHelper.java
@@ -6,9 +6,7 @@ public class DelayWeatherCorrelationHelper
 	{
 		return new StringBuilder("SELECT ").append(what)
 				.append(", (UNIX_TIMESTAMP(Stop.realTime) - UNIX_TIMESTAMP(Stop.timeTabledTime)) AS delay ")
-				.append("FROM StopWeather, Stop, Weather, Station ")
-				.append("WHERE Stop.stopID = StopWeather.stopID AND Stop.stationID = Station.stationID ")
-				.append("AND Weather.lat = ROUND(Station.lat, 2) AND Weather.lon = ROUND(Station.lon, 2) ")
-				.append("AND Weather.timeStamp = StopWeather.timeStamp;").toString();
+				.append("FROM StopWeather, Stop, Weather ")
+				.append("WHERE Stop.stopID = StopWeather.stopID AND StopWeather.weatherId = Weather.id;").toString();
 	}
 }
