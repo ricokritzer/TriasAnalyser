369942867c3ed974424e0383ce60520840525259
ricokritzer <rico@kritzer.net>
2019-02-25 01:34:03 +0100

    Verknüpfung von Stop und Weather im Text verbessert

--- Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/reader/database/DelayWeatherTextDB.java
+++ Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/reader/database/DelayWeatherTextDB.java
@@ -80,12 +80,9 @@ public class DelayWeatherTextDB
 				"max(UNIX_TIMESTAMP(Stop.realTime) - UNIX_TIMESTAMP(Stop.timeTabledTime)) AS delay_max, " +
 				"WeatherIcon.textDE, " +
 				"WeatherIcon.icon " +
-				"FROM StopWeather, Stop, Weather, Station, WeatherIcon " +
+				"FROM StopWeather, Stop, Weather, WeatherIcon " +
 				"WHERE Stop.stopID = StopWeather.stopID " +
-				"AND Stop.stationID = Station.stationID " +
-				"AND Weather.lat = ROUND(Station.lat, 2) " +
-				"AND Weather.lon = ROUND(Station.lon, 2) " +
-				"AND Weather.timeStamp = StopWeather.timeStamp " +
+				"AND StopWeather.weatherId = Weather.id " +
 				"AND WeatherIcon.text = Weather.text " +
 				"GROUP BY WeatherIcon.textDE, WeatherIcon.icon;";
 	}
