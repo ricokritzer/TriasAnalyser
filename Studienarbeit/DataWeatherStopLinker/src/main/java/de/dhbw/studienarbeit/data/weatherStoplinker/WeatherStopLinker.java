2e26a8ed1171e74472adc463fc59e05228b63080
ricokritzer <rico@kritzer.net>
2019-02-25 01:58:48 +0100

    Refactoring

--- Studienarbeit/DataWeatherStopLinker/src/main/java/de/dhbw/studienarbeit/data/weatherStoplinker/WeatherStopLinker.java
+++ Studienarbeit/DataWeatherStopLinker/src/main/java/de/dhbw/studienarbeit/data/weatherStoplinker/WeatherStopLinker.java
@@ -1,53 +1,34 @@
 package de.dhbw.studienarbeit.data.weatherStoplinker;
 
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.util.Date;
 import java.util.Timer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import de.dhbw.studienarbeit.data.helper.database.saver.DatabaseSaver;
-import de.dhbw.studienarbeit.data.helper.database.saver.Saveable;
 import de.dhbw.studienarbeit.data.helper.datamanagement.MyTimerTask;
 import de.dhbw.studienarbeit.data.reader.database.Count;
 
-public class WeatherStopLinker implements Saveable
+public class WeatherStopLinker
 {
 	private static final Logger LOGGER = Logger.getLogger(WeatherStopLinker.class.getName());
-	private static long idx = 1;
+
+	private long idx = 1;
 
 	public static void main(String[] args)
 	{
-		Timer timer = new Timer();
-		timer.schedule(new MyTimerTask(() -> new WeatherStopLinker().linkWeatherAndStops()), new Date(),
-				60 * 60 * 1000l); // every 60 Minutes
+		new WeatherStopLinker().linkWeatherAndStops();
 	}
 
 	private void linkWeatherAndStops()
 	{
-		Count stopCount = Count.countStops();
+		final Count stopCount = Count.countStops();
 		while (idx <= stopCount.getValue())
 		{
-			DatabaseSaver.saveData(this);
-			LOGGER.log(Level.FINE, "Stop with index " + idx + " linked to weather.");
+			DatabaseSaver.saveData(new StopWeather(idx));
+			LOGGER.log(Level.INFO, "Stop with index " + idx + " linked to weather.");
 			idx++;
 		}
-	}
 
-	@Override
-	public String getSQLQuerry()
-	{
-		return "INSERT INTO StopWeather SELECT stopID, Weather.timeStamp, Weather.id FROM Stop, Station, Weather "
-				+ "WHERE Stop.stationID = Station.stationID AND Stop.stopID = ? AND Stop.stopID NOT IN (SELECT stopID FROM StopWeather) "
-				+ "AND Weather.lat = ROUND(Station.lat, 2) AND Weather.lon = ROUND(Station.lon, 2) AND Weather.timeStamp < Stop.realTime "
-				+ "GROUP BY Stop.stopID, Stop.realTime, Weather.timeStamp, Weather.id "
-				+ "HAVING count(Weather.timeStamp) > 0 AND max(Weather.timeStamp) > DATE_SUB(Stop.realTime,INTERVAL 30 MINUTE);";
-	}
-
-	@Override
-	public void setValues(PreparedStatement preparedStatement) throws SQLException
-	{
-		preparedStatement.setLong(1, idx);
+		new Timer().schedule(new MyTimerTask(this::linkWeatherAndStops), 60 * 60 * 1000l);
 	}
 }
