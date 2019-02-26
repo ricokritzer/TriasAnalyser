44d0f69a7aca56596cf03db2d23fd075599ef8a0
ricokritzer <rico@kritzer.net>
2019-02-26 11:03:09 +0100

    Multithreading

--- Studienarbeit/DataWeatherStopLinker/src/main/java/de/dhbw/studienarbeit/data/weatherStoplinker/WeatherStopLinker.java
+++ Studienarbeit/DataWeatherStopLinker/src/main/java/de/dhbw/studienarbeit/data/weatherStoplinker/WeatherStopLinker.java
@@ -1,5 +1,7 @@
 package de.dhbw.studienarbeit.data.weatherStoplinker;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Timer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -10,6 +12,7 @@ import de.dhbw.studienarbeit.data.reader.database.Count;
 public class WeatherStopLinker
 {
 	private static final Logger LOGGER = Logger.getLogger(WeatherStopLinker.class.getName());
+	private static final int MAXIMUM_CONNECTIONS = 5;
 
 	public static void main(String[] args)
 	{
@@ -18,15 +21,55 @@ public class WeatherStopLinker
 
 	private void link()
 	{
+		final long start = 1;
 		final long end = Count.countStops().getValue();
 
-		LOGGER.log(Level.INFO, "Linker starts with: " + end);
-
-		for (long i = end; i > 0; i--)
-		{
-			new StopWeather(i).save();
-		}
+		LOGGER.log(Level.FINE, "Linker starts with: " + end);
+		linkAsync(start, end);
 
 		new Timer().schedule(new MyTimerTask(this::link), 60 * 60 * 1000l);
+	}
+
+	private void linkAsync(long start, long end)
+	{
+		long distance = end - start;
+		long countPerConnection = distance / MAXIMUM_CONNECTIONS;
+
+		final List<Thread> threads = new ArrayList<>();
+		for (int i = 0; i < MAXIMUM_CONNECTIONS; i++)
+		{
+			final int idx = i;
+			threads.add(new Thread(() -> link(countPerConnection * idx + 1, countPerConnection * (idx + 1))));
+		}
+		threads.forEach(Thread::start);
+		threads.forEach(this::waitFor);
+	}
+
+	private void link(long start, long end)
+	{
+		for (long i = end; i >= start; i--)
+		{
+			link(i);
+		}
+	}
+
+	private void waitFor(Thread t)
+	{
+		try
+		{
+			t.join();
+		}
+		catch (InterruptedException e)
+		{
+			LOGGER.log(Level.WARNING, e.getMessage(), e);
+		}
+	}
+
+	private void link(long idx)
+	{
+		if (idx > 1)
+		{
+			new StopWeather(idx).save();
+		}
 	}
 }
