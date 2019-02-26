b2bb9af333a821a6ed3c3eaf88cb96c1e5149d8e
ricokritzer <rico@kritzer.net>
2019-02-26 09:14:56 +0100

    Fehlerbehebung: Speicherung von Verknüpfungen

--- Studienarbeit/DataWeatherStopLinker/src/main/java/de/dhbw/studienarbeit/data/weatherStoplinker/WeatherStopLinker.java
+++ Studienarbeit/DataWeatherStopLinker/src/main/java/de/dhbw/studienarbeit/data/weatherStoplinker/WeatherStopLinker.java
@@ -1,12 +1,16 @@
 package de.dhbw.studienarbeit.data.weatherStoplinker;
 
 import java.util.Timer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import de.dhbw.studienarbeit.data.helper.datamanagement.MyTimerTask;
 import de.dhbw.studienarbeit.data.reader.database.Count;
 
 public class WeatherStopLinker
 {
+	private static final Logger LOGGER = Logger.getLogger(WeatherStopLinker.class.getName());
+
 	public static void main(String[] args)
 	{
 		new WeatherStopLinker().link();
@@ -16,9 +20,11 @@ public class WeatherStopLinker
 	{
 		final long end = Count.countStops().getValue();
 
+		LOGGER.log(Level.INFO, "Linker starts with: " + end);
+
 		for (long i = end; i > 0; i--)
 		{
-			new StopWeather(i);
+			new StopWeather(i).save();
 		}
 
 		new Timer().schedule(new MyTimerTask(this::link), 60 * 60 * 1000l);
