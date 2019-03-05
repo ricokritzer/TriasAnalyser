331d1e2db7ed41357878c7ada737c7296904caab
ricokritzer <rico@kritzer.net>
2019-03-05 18:40:17 +0100

    Fehlerbehebung: Konstruktoren angepasst

--- Studienarbeit/DataReader/src/test/java/de/dhbw/studienarbeit/data/reader/database/DelayStationNeighbourDBTest.java
+++ Studienarbeit/DataReader/src/test/java/de/dhbw/studienarbeit/data/reader/database/DelayStationNeighbourDBTest.java
@@ -10,35 +10,35 @@ public class DelayStationNeighbourDBTest
 	@Test
 	void testSlope() throws Exception
 	{
-		final DelayStationNeighbourDB stationNeighbourDB = new DelayStationNeighbourDB(0, 0, 0, 3, 4, 1);
+		final DelayStationNeighbourDB stationNeighbourDB = new DelayStationNeighbourDB("foo", 0, 0, 0, "bar", 3, 4, 1);
 		assertThat(stationNeighbourDB.getSlope(), Is.is(0.2));
 	}
 
 	@Test
 	void testSlopeNegative() throws Exception
 	{
-		final DelayStationNeighbourDB stationNeighbourDB = new DelayStationNeighbourDB(3, 4, 0, 0, 0, 1);
+		final DelayStationNeighbourDB stationNeighbourDB = new DelayStationNeighbourDB("foo", 3, 4, 0, "bar", 0, 0, 1);
 		assertThat(stationNeighbourDB.getSlope(), Is.is(0.2));
 	}
 
 	@Test
 	void testSlopeFive() throws Exception
 	{
-		final DelayStationNeighbourDB stationNeighbourDB = new DelayStationNeighbourDB(3, 4, 0, 0, 0, 5);
+		final DelayStationNeighbourDB stationNeighbourDB = new DelayStationNeighbourDB("foo", 3, 4, 0, "bar", 0, 0, 5);
 		assertThat(stationNeighbourDB.getSlope(), Is.is(1.0));
 	}
 
 	@Test
 	void testSlopeLatLonDoesNotMatter() throws Exception
 	{
-		final DelayStationNeighbourDB stationNeighbourDB = new DelayStationNeighbourDB(4, 3, 0, 0, 0, 5);
+		final DelayStationNeighbourDB stationNeighbourDB = new DelayStationNeighbourDB("foo", 4, 3, 0, "bar", 0, 0, 5);
 		assertThat(stationNeighbourDB.getSlope(), Is.is(1.0));
 	}
 
 	@Test
 	void testSlope0() throws Exception
 	{
-		final DelayStationNeighbourDB stationNeighbourDB = new DelayStationNeighbourDB(0, 0, 0, 0, 0, 0);
+		final DelayStationNeighbourDB stationNeighbourDB = new DelayStationNeighbourDB("foo", 0, 0, 0, "bar", 0, 0, 0);
 		assertThat(stationNeighbourDB.getSlope(), Is.is(Double.NaN));
 	}
 }
