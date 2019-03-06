e18c796b5d460d579f488c382eea064d679d6b26
ricokritzer <rico@kritzer.net>
2019-03-06 13:41:25 +0100

    Anpassung der WO-Objekte bei der Korrelation (Typsicherheit)

--- Studienarbeit/WebData/src/main/java/de/dhbw/studienarbeit/web/data/weather/clouds/DelayCloudsCorrelationCoefficientWO.java
+++ Studienarbeit/WebData/src/main/java/de/dhbw/studienarbeit/web/data/weather/clouds/DelayCloudsCorrelationCoefficientWO.java
@@ -3,27 +3,34 @@ package de.dhbw.studienarbeit.web.data.weather.clouds;
 import java.io.IOException;
 import java.util.Optional;
 
+import de.dhbw.studienarbeit.data.reader.data.weather.DelayCloudsCorrelation;
 import de.dhbw.studienarbeit.data.reader.database.DelayCloudCorrelationDB;
 import de.dhbw.studienarbeit.web.data.update.DataUpdater;
 import de.dhbw.studienarbeit.web.data.update.Updateable;
 
 public class DelayCloudsCorrelationCoefficientWO extends Updateable
 {
-	private double data = 0.0;
+	private DelayCloudsCorrelation data = new DelayCloudsCorrelation(0.0);
 
 	public DelayCloudsCorrelationCoefficientWO(Optional<DataUpdater> updater)
 	{
 		updater.ifPresent(u -> u.updateEvery(1, DAYS, this));
 	}
 
-	public double getData()
+	public DelayCloudsCorrelation getCorrelation()
 	{
 		return data;
+	}
+
+	@Deprecated
+	public double getData()
+	{
+		return data.getDelayCloudsCorrelation();
 	}
 
 	@Override
 	protected void updateData() throws IOException
 	{
-		data = DelayCloudCorrelationDB.getCorrelationCoefficient();
+		data = DelayCloudCorrelationDB.getDelayCloudsCorrelation();
 	}
 }
--- Studienarbeit/WebData/src/main/java/de/dhbw/studienarbeit/web/data/weather/temperature/DelayTemperatureCorrelationCoefficientWO.java
+++ Studienarbeit/WebData/src/main/java/de/dhbw/studienarbeit/web/data/weather/temperature/DelayTemperatureCorrelationCoefficientWO.java
@@ -3,27 +3,34 @@ package de.dhbw.studienarbeit.web.data.weather.temperature;
 import java.io.IOException;
 import java.util.Optional;
 
+import de.dhbw.studienarbeit.data.reader.data.weather.DelayTemperatureCorrelation;
 import de.dhbw.studienarbeit.data.reader.database.DelayTempCorrelationDB;
 import de.dhbw.studienarbeit.web.data.update.DataUpdater;
 import de.dhbw.studienarbeit.web.data.update.Updateable;
 
 public class DelayTemperatureCorrelationCoefficientWO extends Updateable
 {
-	private double data = 0.0;
+	private DelayTemperatureCorrelation data = new DelayTemperatureCorrelation(0.0);
 
 	public DelayTemperatureCorrelationCoefficientWO(Optional<DataUpdater> updater)
 	{
 		updater.ifPresent(u -> u.updateEvery(1, DAYS, this));
 	}
 
-	public double getData()
+	public DelayTemperatureCorrelation getCorrelation()
 	{
 		return data;
+	}
+
+	@Deprecated
+	public double getData()
+	{
+		return data.getDelayCloudsCorrelation();
 	}
 
 	@Override
 	protected void updateData() throws IOException
 	{
-		data = DelayTempCorrelationDB.getCorrelationCoefficient();
+		data = DelayTempCorrelationDB.getDelayTemperatureCorrelation();
 	}
 }
