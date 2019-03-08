d8068444194a76c1ec5bf6f253487ba008d7b9e0
ricokritzer <rico@kritzer.net>
2019-03-08 10:34:50 +0100

    Deutsche Bezeichnung der Wochentage ergänzt

--- Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/reader/data/time/Weekday.java
+++ Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/reader/data/time/Weekday.java
@@ -2,5 +2,23 @@ package de.dhbw.studienarbeit.data.reader.data.time;
 
 public enum Weekday
 {
-	MONDAY, TUESDAY, WEDNESDAY, THIRSTDAY, FRIDAY, SATURDAY, SUNDAY
+	MONDAY("Montag"), //
+	TUESDAY("Dienstag"), //
+	WEDNESDAY("Mittwoch"), //
+	THURSDAY("Donnerstag"), //
+	FRIDAY("Freitag"), //
+	SATURDAY("Samstag"), // ,
+	SUNDAY("Sonntag");
+
+	private String name;
+
+	private Weekday(String name)
+	{
+		this.name = name;
+	}
+
+	public String getName()
+	{
+		return name;
+	}
 }
