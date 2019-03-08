c8c44023571df855a651d6127d6ff95df96d9634
ricokritzer <rico@kritzer.net>
2019-03-08 18:21:12 +0100

    Schwer testbare Methoden auf deprecated gesetzt.

--- Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/reader/data/count/CountDB.java
+++ Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/reader/data/count/CountDB.java
@@ -6,6 +6,9 @@ import java.util.Optional;
 
 public class CountDB
 {
+	private CountDB()
+	{}
+
 	public static final Optional<Count> getCount(ResultSet result)
 	{
 		try
@@ -18,41 +21,49 @@ public class CountDB
 		}
 	}
 
+	@Deprecated
 	public static final Count countStops()
 	{
 		return new CountStopsDB().countStops();
 	}
 
+	@Deprecated
 	public static final Count countWeather()
 	{
 		return new CountWeatherDB().countWeather();
 	}
 
+	@Deprecated
 	public static final Count countStations()
 	{
 		return new CountStationsDB().countStations();
 	}
 
+	@Deprecated
 	public static final Count countStationsWithRealtimeData()
 	{
 		return new CountStationsDB().countStationsWithRealtimeData();
 	}
 
+	@Deprecated
 	public static final Count countObservedStations()
 	{
 		return new CountStationsDB().countObservedStations();
 	}
 
+	@Deprecated
 	public static final Count countLines()
 	{
 		return new CountLinesDB().countLines();
 	}
 
+	@Deprecated
 	public static final Count countOperators()
 	{
 		return new CountOperatorsDB().countOperators();
 	}
 
+	@Deprecated
 	public static final Count countObservedOperators()
 	{
 		return new CountOperatorsDB().countObservedOperators();
