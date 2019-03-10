fb34f3dc60caadd3541ac1fcf16bff4c9e769afb
Patrick <pat-sie@web.de>
2019-03-10 18:27:20 +0100

    TemperaturDiv lesbar + deprecated Methoden entfernt

--- Studienarbeit/WebView/src/main/java/de/dhbw/studienarbeit/WebView/components/DelayTemperatureDiv.java
+++ Studienarbeit/WebView/src/main/java/de/dhbw/studienarbeit/WebView/components/DelayTemperatureDiv.java
@@ -16,10 +16,6 @@ public class DelayTemperatureDiv extends Div
 {
 	private static final long serialVersionUID = 6L;
 
-	private static final int SECONDS_PER_MINUTE = 60;
-	private static final int SECONDS_PER_HOUR = SECONDS_PER_MINUTE * 60;
-	private static final int SECONDS_PER_DAY = SECONDS_PER_HOUR * 24;
-
 	private final Grid<DelayTemperatureData> grid = new Grid<>();
 	private final TextField field = new TextField();
 
@@ -36,12 +32,12 @@ public class DelayTemperatureDiv extends Div
 		field.setValue(new SimpleDateFormat("dd.MM.yyyy HH:mm:ss").format(Data.getDelayTemperatureWO().getLastUpdated()));
 		layout.add(field);
 
-		grid.addColumn(db -> db.getTemperature()).setHeader("Temperatur")
-				.setComparator((db1, db2) -> Double.compare(db1.getTemperature(), db2.getTemperature())).setSortable(true);
-		grid.addColumn(db -> convertTimeToString(db.getDelayAverage())).setHeader("Durchschnitt")
-				.setComparator((db1, db2) -> Double.compare(db1.getDelayAverage(), db2.getDelayAverage())).setSortable(true);
-		grid.addColumn(db -> convertTimeToString(db.getDelayMaximum())).setHeader("Maximal")
-				.setComparator((db1, db2) -> Double.compare(db1.getDelayMaximum(), db2.getDelayMaximum())).setSortable(true);
+		grid.addColumn(db -> db.getValue()).setHeader("Temperatur")
+				.setComparator((db1, db2) -> Double.compare(db1.getValue(), db2.getValue())).setSortable(true);
+		grid.addColumn(db -> db.getAverage().toString()).setHeader("Durchschnitt")
+				.setComparator((db1, db2) -> Double.compare(db1.getAverage().getValue(), db2.getAverage().getValue())).setSortable(true);
+		grid.addColumn(db -> db.getMaximum().toString()).setHeader("Maximal")
+				.setComparator((db1, db2) -> Double.compare(db1.getMaximum().getValue(), db2.getMaximum().getValue())).setSortable(true);
 
 		grid.setSizeFull();
 		grid.setSelectionMode(SelectionMode.NONE);
@@ -51,30 +47,5 @@ public class DelayTemperatureDiv extends Div
 		add(layout);
 
 		setVisible(false);
-	}
-
-	private String convertTimeToString(double time)
-	{
-		if (time > SECONDS_PER_DAY)
-		{
-			final double days = time / SECONDS_PER_DAY;
-			return Double.toString(round(days)) + " Tage";
-		}
-		if (time > SECONDS_PER_HOUR)
-		{
-			final double hours = time / SECONDS_PER_HOUR;
-			return Double.toString(round(hours)) + " Stunden";
-		}
-		if (time > SECONDS_PER_MINUTE)
-		{
-			final double minutes = time / SECONDS_PER_MINUTE;
-			return Double.toString(round(minutes)) + " Minuten";
-		}
-		return Double.toString(time) + " Sekunden";
-	}
-
-	private double round(double value)
-	{
-		return Math.round(value * 100) / 100.0;
 	}
 }
