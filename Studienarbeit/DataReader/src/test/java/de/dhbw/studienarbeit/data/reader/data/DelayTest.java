377b62c523d76108c58b161593f29429a468d8bc
Patrick <pat-sie@web.de>
2019-03-10 18:17:33 +0100

    Hab die Tests ausversehen mit JUnit5 geschrieben, wir haben aber JUnit4

--- Studienarbeit/DataReader/src/test/java/de/dhbw/studienarbeit/data/reader/data/DelayTest.java
+++ Studienarbeit/DataReader/src/test/java/de/dhbw/studienarbeit/data/reader/data/DelayTest.java
@@ -1,33 +1,30 @@
-package de.dhbw.studienarbeit.WebView.util;
+package de.dhbw.studienarbeit.data.reader.data;
 
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-import de.dhbw.studienarbeit.data.reader.data.Delay;
-
-class DisplayHelperTest
+public class DelayTest
 {
-
 	@Test
 	void sekundenWerdenRichtigKonvertiert()
 	{
 		assertThat(new Delay(11.111).toString(), is("11.11s"));
 	}
-	
+
 	@Test
 	void sekundenWerdenRichtigAufgerundet()
 	{
 		assertThat(new Delay(11.115).toString(), is("11.12s"));
 	}
-	
+
 	@Test
 	void MinutenWerdenRichtigKonvertiert()
 	{
 		assertThat(new Delay(65.123).toString(), is("1m 5s"));
 	}
-	
+
 	@Test
 	void StundenWerdenRichtigKonvertiert()
 	{
