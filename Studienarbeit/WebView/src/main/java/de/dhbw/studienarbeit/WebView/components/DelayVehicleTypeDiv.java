2b65a9d4efd7eb6bd805000ca58fa2b04e0acbb2
Patrick <pat-sie@web.de>
2019-03-10 18:29:03 +0100

    VehicleTypeDiv lesbar + deprecated Methoden entfernt

--- Studienarbeit/WebView/src/main/java/de/dhbw/studienarbeit/WebView/components/DelayVehicleTypeDiv.java
+++ Studienarbeit/WebView/src/main/java/de/dhbw/studienarbeit/WebView/components/DelayVehicleTypeDiv.java
@@ -16,10 +16,6 @@ public class DelayVehicleTypeDiv extends Div
 {
 	private static final long serialVersionUID = 5L;
 
-	private static final int SECONDS_PER_MINUTE = 60;
-	private static final int SECONDS_PER_HOUR = SECONDS_PER_MINUTE * 60;
-	private static final int SECONDS_PER_DAY = SECONDS_PER_HOUR * 24;
-
 	private final Grid<DelayVehicleTypeData> grid = new Grid<>();
 	private final TextField field = new TextField();
 
@@ -37,10 +33,10 @@ public class DelayVehicleTypeDiv extends Div
 		layout.add(field);
 
 		grid.addColumn(db -> db.getVehicleType()).setHeader("Fahrzeugtyp").setSortable(true);
-		grid.addColumn(db -> convertTimeToString(db.getDelayAverage())).setHeader("Durchschnitt")
-				.setComparator((db1, db2) -> Double.compare(db1.getDelayAverage(), db2.getDelayAverage())).setSortable(true);
-		grid.addColumn(db -> convertTimeToString(db.getDelayMaximum())).setHeader("Maximal")
-				.setComparator((db1, db2) -> Double.compare(db1.getDelayMaximum(), db2.getDelayMaximum())).setSortable(true);
+		grid.addColumn(db -> db.getAverage().toString()).setHeader("Durchschnitt")
+				.setComparator((db1, db2) -> Double.compare(db1.getAverage().getValue(), db2.getAverage().getValue())).setSortable(true);
+		grid.addColumn(db -> db.getMaximum().toString()).setHeader("Maximal")
+				.setComparator((db1, db2) -> Double.compare(db1.getMaximum().getValue(), db2.getMaximum().getValue())).setSortable(true);
 
 		grid.setSizeFull();
 		grid.setSelectionMode(SelectionMode.NONE);
@@ -50,30 +46,5 @@ public class DelayVehicleTypeDiv extends Div
 		add(layout);
 
 		setVisible(false);
-	}
-
-	private String convertTimeToString(double time)
-	{
-		if (time > SECONDS_PER_DAY)
-		{
-			final double days = time / SECONDS_PER_DAY;
-			return Double.toString(round(days)) + " Tage";
-		}
-		if (time > SECONDS_PER_HOUR)
-		{
-			final double hours = time / SECONDS_PER_HOUR;
-			return Double.toString(round(hours)) + " Stunden";
-		}
-		if (time > SECONDS_PER_MINUTE)
-		{
-			final double minutes = time / SECONDS_PER_MINUTE;
-			return Double.toString(round(minutes)) + " Minuten";
-		}
-		return Double.toString(time) + " Sekunden";
-	}
-
-	private double round(double value)
-	{
-		return Math.round(value * 100) / 100.0;
 	}
 }
