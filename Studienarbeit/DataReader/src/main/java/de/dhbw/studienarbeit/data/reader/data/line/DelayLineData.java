4fe1295f64b23f5715339de5b47ae9a61b41a159
ricokritzer <rico@kritzer.net>
2019-03-12 18:32:03 +0100

    Refactoring: Implementierung eines alten Interfaces entfernt

--- Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/reader/data/line/DelayLineData.java
+++ Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/reader/data/line/DelayLineData.java
@@ -4,38 +4,33 @@ import de.dhbw.studienarbeit.data.reader.data.DelayAverage;
 import de.dhbw.studienarbeit.data.reader.data.DelayData;
 import de.dhbw.studienarbeit.data.reader.data.DelayMaximum;
 
-public class DelayLineData extends DelayData<Line> implements Line
+public class DelayLineData extends DelayData<Line>
 {
 	public DelayLineData(DelayMaximum delayMaximum, DelayAverage delayAverage, Line line)
 	{
 		super(delayMaximum, delayAverage, line);
 	}
 
-	@Override
 	public LineID getID()
 	{
 		return value.getID();
 	}
 
-	@Override
 	public LineName getName()
 	{
 		return value.getName();
 	}
 
-	@Override
 	public LineDestination getDestination()
 	{
 		return value.getDestination();
 	}
 
-	@Override
 	public String getLineName()
 	{
 		return value.getLineName();
 	}
 
-	@Override
 	public String getLineDestination()
 	{
 		return value.getLineDestination();
