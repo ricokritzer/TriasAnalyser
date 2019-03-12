9ce34a819aa3535668c259850e5b14404cd53f7e
ricokritzer <rico@kritzer.net>
2019-03-12 18:33:52 +0100

    Methoden auf Deprecated gesetzt

--- Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/reader/data/line/DelayLineData.java
+++ Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/reader/data/line/DelayLineData.java
@@ -11,26 +11,46 @@ public class DelayLineData extends DelayData<Line>
 		super(delayMaximum, delayAverage, line);
 	}
 
+	/*
+	 * @Deprecated use getValue()
+	 */
+	@Deprecated
 	public LineID getID()
 	{
 		return value.getID();
 	}
 
+	/*
+	 * @Deprecated use getValue()
+	 */
+	@Deprecated
 	public LineName getName()
 	{
 		return value.getName();
 	}
 
+	/*
+	 * @Deprecated use getValue()
+	 */
+	@Deprecated
 	public LineDestination getDestination()
 	{
 		return value.getDestination();
 	}
 
+	/*
+	 * @Deprecated use getValue()
+	 */
+	@Deprecated
 	public String getLineName()
 	{
 		return value.getLineName();
 	}
 
+	/*
+	 * @Deprecated use getValue()
+	 */
+	@Deprecated
 	public String getLineDestination()
 	{
 		return value.getLineDestination();
