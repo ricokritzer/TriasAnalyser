ff7f2b1b4e67634b1749d2b107714d9cf6932c6a
ricokritzer <rico@kritzer.net>
2019-03-12 19:29:03 +0100

    Deprecated Methoden ausgebaut

--- Studienarbeit/DataReader/src/test/java/de/dhbw/studienarbeit/data/reader/data/station/DelayStationNeighbourDBTest.java
+++ Studienarbeit/DataReader/src/test/java/de/dhbw/studienarbeit/data/reader/data/station/DelayStationNeighbourDBTest.java
@@ -5,6 +5,8 @@ import static org.junit.Assert.assertThat;
 import org.hamcrest.core.Is;
 import org.junit.jupiter.api.Test;
 
+import de.dhbw.studienarbeit.data.reader.data.DelayAverage;
+
 public class DelayStationNeighbourDBTest
 {
 	private final StationName name = new StationName("foo");
@@ -14,7 +16,11 @@ public class DelayStationNeighbourDBTest
 	{
 		final Position pos1 = new Position(0, 0);
 		final Position pos2 = new Position(3, 4);
-		final DelayStationNeighbourData stationNeighbour = new DelayStationNeighbourData(name, pos1, 0, name, pos2, 1);
+		final DelayAverage avg1 = new DelayAverage(0.0);
+		final DelayAverage avg2 = new DelayAverage(1.0);
+
+		final DelayStationNeighbourData stationNeighbour = new DelayStationNeighbourData(name, pos1, avg1, name, pos2,
+				avg2);
 
 		assertThat(stationNeighbour.getSlope(), Is.is(0.2));
 	}
@@ -24,7 +30,11 @@ public class DelayStationNeighbourDBTest
 	{
 		final Position pos1 = new Position(3, 4);
 		final Position pos2 = new Position(0, 0);
-		final DelayStationNeighbourData stationNeighbour = new DelayStationNeighbourData(name, pos1, 0, name, pos2, 1);
+		final DelayAverage avg1 = new DelayAverage(0.0);
+		final DelayAverage avg2 = new DelayAverage(1.0);
+
+		final DelayStationNeighbourData stationNeighbour = new DelayStationNeighbourData(name, pos1, avg1, name, pos2,
+				avg2);
 
 		assertThat(stationNeighbour.getSlope(), Is.is(0.2));
 	}
@@ -34,7 +44,11 @@ public class DelayStationNeighbourDBTest
 	{
 		final Position pos1 = new Position(3, 4);
 		final Position pos2 = new Position(0, 0);
-		final DelayStationNeighbourData stationNeighbour = new DelayStationNeighbourData(name, pos1, 0, name, pos2, 5);
+		final DelayAverage avg1 = new DelayAverage(0.0);
+		final DelayAverage avg2 = new DelayAverage(5.0);
+
+		final DelayStationNeighbourData stationNeighbour = new DelayStationNeighbourData(name, pos1, avg1, name, pos2,
+				avg2);
 
 		assertThat(stationNeighbour.getSlope(), Is.is(1.0));
 	}
@@ -44,7 +58,11 @@ public class DelayStationNeighbourDBTest
 	{
 		final Position pos1 = new Position(4, 3);
 		final Position pos2 = new Position(0, 0);
-		final DelayStationNeighbourData stationNeighbour = new DelayStationNeighbourData(name, pos1, 0, name, pos2, 5);
+		final DelayAverage avg1 = new DelayAverage(0.0);
+		final DelayAverage avg2 = new DelayAverage(5.0);
+
+		final DelayStationNeighbourData stationNeighbour = new DelayStationNeighbourData(name, pos1, avg1, name, pos2,
+				avg2);
 
 		assertThat(stationNeighbour.getSlope(), Is.is(1.0));
 	}
@@ -54,8 +72,11 @@ public class DelayStationNeighbourDBTest
 	{
 		final Position pos1 = new Position(0, 0);
 		final Position pos2 = new Position(0, 0);
-		final DelayStationNeighbourData stationNeighbourData = new DelayStationNeighbourData(name, pos1, 0, name, pos2,
-				0);
+		final DelayAverage avg1 = new DelayAverage(0.0);
+		final DelayAverage avg2 = new DelayAverage(0.0);
+
+		final DelayStationNeighbourData stationNeighbourData = new DelayStationNeighbourData(name, pos1, avg1, name,
+				pos2, avg2);
 
 		assertThat(stationNeighbourData.getSlope(), Is.is(Double.NaN));
 	}
