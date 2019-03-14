64b32e42d1ec9eba4b2128b0f4fc225099ab47ca
ricokritzer <rico@kritzer.net>
2019-03-14 08:35:35 +0100

    Refactoring: Codeduplikate entfernt (ApiKeyDB)

--- Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/reader/data/api/ApiKeyDB.java
+++ Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/reader/data/api/ApiKeyDB.java
@@ -7,33 +7,14 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import de.dhbw.studienarbeit.data.helper.datamanagement.ApiKeyData;
 import de.dhbw.studienarbeit.data.reader.data.operator.OperatorID;
+import de.dhbw.studienarbeit.data.reader.database.DB;
 import de.dhbw.studienarbeit.data.reader.database.DatabaseReader;
 
-public class ApiKeyDB implements ApiKey
+public class ApiKeyDB extends DB<ApiKeyData> implements ApiKey
 {
-	private static final Logger LOGGER = Logger.getLogger(ApiKeyDB.class.getName());
-
-	private static final Optional<ApiKeyData> getApiKey(ResultSet result)
-	{
-		try
-		{
-			final String key = result.getString("apiKey");
-			final int requests = result.getInt("maximumRequests");
-			final String url = result.getString("url");
-			return Optional.of(new ApiKeyData(key, requests, url));
-		}
-		catch (SQLException e)
-		{
-			LOGGER.log(Level.WARNING, "Unable to parse to api key.", e);
-			return Optional.empty();
-		}
-	}
-
 	public final List<ApiKeyData> getApiKeys(OperatorID operator) throws IOException
 	{
 		final String sql = "SELECT * FROM Api WHERE name = ?;";
@@ -43,12 +24,21 @@ public class ApiKeyDB implements ApiKey
 			preparedStatement.setString(1, operator.getName());
 
 			final List<ApiKeyData> list = new ArrayList<>();
-			database.select(r -> getApiKey(r).ifPresent(list::add), preparedStatement);
+			database.select(r -> parse(r).ifPresent(list::add), preparedStatement);
 			return list;
 		}
 		catch (SQLException e)
 		{
 			throw new IOException("Unable to select apis by name " + operator.getName(), e);
 		}
+	}
+
+	@Override
+	protected Optional<ApiKeyData> getValue(ResultSet result) throws SQLException
+	{
+		final String key = result.getString("apiKey");
+		final int requests = result.getInt("maximumRequests");
+		final String url = result.getString("url");
+		return Optional.of(new ApiKeyData(key, requests, url));
 	}
 }
