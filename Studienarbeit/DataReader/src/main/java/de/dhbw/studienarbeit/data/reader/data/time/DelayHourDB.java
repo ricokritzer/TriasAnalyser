54873700b51911d59f68272894882f1b697522b3
ricokritzer <rico@kritzer.net>
2019-03-14 08:53:28 +0100

    Refactoring: Codeduplikate entfernt (DelayHourDB)

--- Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/reader/data/time/DelayHourDB.java
+++ Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/reader/data/time/DelayHourDB.java
@@ -7,34 +7,14 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import de.dhbw.studienarbeit.data.reader.data.DelayAverage;
 import de.dhbw.studienarbeit.data.reader.data.DelayMaximum;
+import de.dhbw.studienarbeit.data.reader.database.DB;
 import de.dhbw.studienarbeit.data.reader.database.DatabaseReader;
 
-public class DelayHourDB implements DelayHour
+public class DelayHourDB extends DB<DelayHourData> implements DelayHour
 {
-	private static final Logger LOGGER = Logger.getLogger(DelayHourDB.class.getName());
-
-	private static final Optional<DelayHourData> getDelays(ResultSet result)
-	{
-		try
-		{
-			final DelayMaximum delayMaximum = new DelayMaximum(result.getDouble("delay_max"));
-			final DelayAverage delayAverage = new DelayAverage(result.getDouble("delay_avg"));
-			final Hour value = Hour.values()[result.getInt("hour")];
-
-			return Optional.of(new DelayHourData(delayMaximum, delayAverage, value));
-		}
-		catch (SQLException e)
-		{
-			LOGGER.log(Level.WARNING, "Unable to parse to " + DelayHourDB.class.getName(), e);
-			return Optional.empty();
-		}
-	}
-
 	public final List<DelayHourData> getDelays() throws IOException
 	{
 		final String sql = "SELECT HOUR(timetabledTime) AS hour, avg(UNIX_TIMESTAMP(realtime) - UNIX_TIMESTAMP(timetabledTime)) AS delay_avg, max(UNIX_TIMESTAMP(realtime) - UNIX_TIMESTAMP(timetabledTime)) AS delay_max FROM Stop GROUP BY hour;";
@@ -42,12 +22,22 @@ public class DelayHourDB implements DelayHour
 		try (PreparedStatement preparedStatement = database.getPreparedStatement(sql))
 		{
 			final List<DelayHourData> list = new ArrayList<>();
-			database.select(r -> DelayHourDB.getDelays(r).ifPresent(list::add), preparedStatement);
+			database.select(r -> parse(r).ifPresent(list::add), preparedStatement);
 			return list;
 		}
 		catch (SQLException e)
 		{
 			throw new IOException("Selecting does not succeed.", e);
 		}
+	}
+
+	@Override
+	protected Optional<DelayHourData> getValue(ResultSet result) throws SQLException
+	{
+		final DelayMaximum delayMaximum = new DelayMaximum(result.getDouble("delay_max"));
+		final DelayAverage delayAverage = new DelayAverage(result.getDouble("delay_avg"));
+		final Hour value = Hour.values()[result.getInt("hour")];
+
+		return Optional.of(new DelayHourData(delayMaximum, delayAverage, value));
 	}
 }
