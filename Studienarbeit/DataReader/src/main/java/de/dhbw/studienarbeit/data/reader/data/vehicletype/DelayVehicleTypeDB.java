6f1db54be5bff809a6bfa287445f1906fe58ae9c
ricokritzer <rico@kritzer.net>
2019-03-14 08:56:21 +0100

    Refactoring: Codeduplikate entfernt (DelayVehicleTypeDB)

--- Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/reader/data/vehicletype/DelayVehicleTypeDB.java
+++ Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/reader/data/vehicletype/DelayVehicleTypeDB.java
@@ -7,34 +7,14 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import de.dhbw.studienarbeit.data.reader.data.DelayAverage;
 import de.dhbw.studienarbeit.data.reader.data.DelayMaximum;
+import de.dhbw.studienarbeit.data.reader.database.DB;
 import de.dhbw.studienarbeit.data.reader.database.DatabaseReader;
 
-public class DelayVehicleTypeDB implements DelayVehicleType
+public class DelayVehicleTypeDB extends DB<DelayVehicleTypeData> implements DelayVehicleType
 {
-	private static final Logger LOGGER = Logger.getLogger(DelayVehicleTypeDB.class.getName());
-
-	private static final Optional<DelayVehicleTypeData> getDelayLine(ResultSet result)
-	{
-		try
-		{
-			final DelayMaximum delayMaximum = new DelayMaximum(result.getDouble("delay_max"));
-			final DelayAverage delayAverage = new DelayAverage(result.getDouble("delay_avg"));
-			final String type = result.getString("type");
-
-			return Optional.of(new DelayVehicleTypeData(delayMaximum, delayAverage, type));
-		}
-		catch (SQLException e)
-		{
-			LOGGER.log(Level.WARNING, "Unable to parse to stop.", e);
-			return Optional.empty();
-		}
-	}
-
 	public final List<DelayVehicleTypeData> getDelays() throws IOException
 	{
 		final String sql = "SELECT " + "SUBSTRING_INDEX(name, ' ', 1 ) AS type, "
@@ -45,12 +25,22 @@ public class DelayVehicleTypeDB implements DelayVehicleType
 		try (PreparedStatement preparedStatement = database.getPreparedStatement(sql))
 		{
 			final List<DelayVehicleTypeData> list = new ArrayList<>();
-			database.select(r -> DelayVehicleTypeDB.getDelayLine(r).ifPresent(list::add), preparedStatement);
+			database.select(r -> parse(r).ifPresent(list::add), preparedStatement);
 			return list;
 		}
 		catch (SQLException e)
 		{
 			throw new IOException("Selecting does not succeed.", e);
 		}
+	}
+
+	@Override
+	protected Optional<DelayVehicleTypeData> getValue(ResultSet result) throws SQLException
+	{
+		final DelayMaximum delayMaximum = new DelayMaximum(result.getDouble("delay_max"));
+		final DelayAverage delayAverage = new DelayAverage(result.getDouble("delay_avg"));
+		final String type = result.getString("type");
+
+		return Optional.of(new DelayVehicleTypeData(delayMaximum, delayAverage, type));
 	}
 }
