44b08fae12180dbed1b9bfddab0881d1408a07a1
ricokritzer <rico@kritzer.net>
2019-03-14 08:46:19 +0100

    Refactoring: Codeduplikate entfernt (DelayStationDB)

--- Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/reader/data/station/DelayStationDB.java
+++ Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/reader/data/station/DelayStationDB.java
@@ -7,38 +7,14 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import de.dhbw.studienarbeit.data.reader.data.DelayAverage;
 import de.dhbw.studienarbeit.data.reader.data.DelayMaximum;
+import de.dhbw.studienarbeit.data.reader.database.DB;
 import de.dhbw.studienarbeit.data.reader.database.DatabaseReader;
 
-public class DelayStationDB implements DelayStation
+public class DelayStationDB extends DB<DelayStationData> implements DelayStation
 {
-	private static final Logger LOGGER = Logger.getLogger(DelayStationDB.class.getName());
-
-	private static final Optional<DelayStationData> getDelayLine(ResultSet result)
-	{
-		try
-		{
-			final DelayMaximum maximum = new DelayMaximum(result.getDouble("delay_max"));
-			final DelayAverage average = new DelayAverage(result.getDouble("delay_avg"));
-			final StationID stationID = new StationID(result.getString("stationID"));
-			final StationName name = new StationName(result.getString("name"));
-			final OperatorName operator = new OperatorName(result.getString("displayName"));
-			final Position position = new Position(result.getDouble("lat"), result.getDouble("lon"));
-			final int count = result.getInt("count");
-
-			return Optional.of(new DelayStationData(maximum, average, stationID, name, operator, position, count));
-		}
-		catch (SQLException e)
-		{
-			LOGGER.log(Level.WARNING, "Unable to parse to DelayStationDB.", e);
-			return Optional.empty();
-		}
-	}
-
 	public final List<DelayStationData> getDelays() throws IOException
 	{
 		final String sql = "SELECT " //
@@ -55,12 +31,26 @@ public class DelayStationDB implements DelayStation
 		try (PreparedStatement preparedStatement = database.getPreparedStatement(sql))
 		{
 			final List<DelayStationData> list = new ArrayList<>();
-			database.select(r -> DelayStationDB.getDelayLine(r).ifPresent(list::add), preparedStatement);
+			database.select(r -> parse(r).ifPresent(list::add), preparedStatement);
 			return list;
 		}
 		catch (SQLException e)
 		{
 			throw new IOException("Selecting does not succeed.", e);
 		}
+	}
+
+	@Override
+	protected Optional<DelayStationData> getValue(ResultSet result) throws SQLException
+	{
+		final DelayMaximum maximum = new DelayMaximum(result.getDouble("delay_max"));
+		final DelayAverage average = new DelayAverage(result.getDouble("delay_avg"));
+		final StationID stationID = new StationID(result.getString("stationID"));
+		final StationName name = new StationName(result.getString("name"));
+		final OperatorName operator = new OperatorName(result.getString("displayName"));
+		final Position position = new Position(result.getDouble("lat"), result.getDouble("lon"));
+		final int count = result.getInt("count");
+
+		return Optional.of(new DelayStationData(maximum, average, stationID, name, operator, position, count));
 	}
 }
