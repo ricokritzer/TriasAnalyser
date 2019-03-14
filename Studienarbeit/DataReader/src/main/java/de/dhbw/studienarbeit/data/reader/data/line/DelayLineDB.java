976a9a6e8b84ff96d9c7e55b7f395e68c6ad0bd4
ricokritzer <rico@kritzer.net>
2019-03-14 08:33:12 +0100

    Refactoring: Codeduplikate entfernt

--- Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/reader/data/line/DelayLineDB.java
+++ Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/reader/data/line/DelayLineDB.java
@@ -7,39 +7,14 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import de.dhbw.studienarbeit.data.reader.data.DelayAverage;
 import de.dhbw.studienarbeit.data.reader.data.DelayMaximum;
+import de.dhbw.studienarbeit.data.reader.database.DB;
 import de.dhbw.studienarbeit.data.reader.database.DatabaseReader;
 
-public class DelayLineDB implements DelayLine
+public class DelayLineDB extends DB<DelayLineData> implements DelayLine
 {
-	private static final Logger LOGGER = Logger.getLogger(DelayLineDB.class.getName());
-
-	private final Optional<DelayLineData> getDelayLine(ResultSet result)
-	{
-		try
-		{
-			final DelayMaximum delayMaximum = new DelayMaximum(result.getDouble("delay_max"));
-			final DelayAverage delayAverage = new DelayAverage(result.getDouble("delay_avg"));
-
-			final LineID lineID = new LineID(result.getInt("lineID"));
-			final LineName name = new LineName(result.getString("name"));
-			final LineDestination destination = new LineDestination(result.getString("destination"));
-
-			final Line line = new LineData(lineID, name, destination);
-
-			return Optional.of(new DelayLineData(delayMaximum, delayAverage, line));
-		}
-		catch (SQLException e)
-		{
-			LOGGER.log(Level.WARNING, "Unable to parse to stop.", e);
-			return Optional.empty();
-		}
-	}
-
 	public final List<DelayLineData> getDelays() throws IOException
 	{
 		final String sql = "SELECT name, destination, Stop.lineID, "
@@ -50,12 +25,27 @@ public class DelayLineDB implements DelayLine
 		try (PreparedStatement preparedStatement = database.getPreparedStatement(sql))
 		{
 			final List<DelayLineData> list = new ArrayList<>();
-			database.select(r -> getDelayLine(r).ifPresent(list::add), preparedStatement);
+			database.select(r -> parse(r).ifPresent(list::add), preparedStatement);
 			return list;
 		}
 		catch (SQLException e)
 		{
 			throw new IOException("Selecting does not succeed.", e);
 		}
+	}
+
+	@Override
+	protected Optional<DelayLineData> getValue(ResultSet result) throws SQLException
+	{
+		final DelayMaximum delayMaximum = new DelayMaximum(result.getDouble("delay_max"));
+		final DelayAverage delayAverage = new DelayAverage(result.getDouble("delay_avg"));
+
+		final LineID lineID = new LineID(result.getInt("lineID"));
+		final LineName name = new LineName(result.getString("name"));
+		final LineDestination destination = new LineDestination(result.getString("destination"));
+
+		final Line line = new LineData(lineID, name, destination);
+
+		return Optional.of(new DelayLineData(delayMaximum, delayAverage, line));
 	}
 }
