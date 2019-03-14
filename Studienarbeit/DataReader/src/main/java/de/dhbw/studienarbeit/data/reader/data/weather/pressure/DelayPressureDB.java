e97797a52c086b8d31dfc8c7576b5f1ef0b11388
ricokritzer <rico@kritzer.net>
2019-03-14 09:02:17 +0100

    Refactoring: Codeduplikate entfernt (DelayPressureDB)

--- Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/reader/data/weather/pressure/DelayPressureDB.java
+++ Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/reader/data/weather/pressure/DelayPressureDB.java
@@ -7,36 +7,17 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import de.dhbw.studienarbeit.data.reader.data.DelayAverage;
 import de.dhbw.studienarbeit.data.reader.data.DelayMaximum;
 import de.dhbw.studienarbeit.data.reader.data.weather.DelayWeatherDBHelper;
+import de.dhbw.studienarbeit.data.reader.database.DB;
 import de.dhbw.studienarbeit.data.reader.database.DatabaseReader;
 
-public class DelayPressureDB implements DelayPressure
+public class DelayPressureDB extends DB<DelayPressureData> implements DelayPressure
 {
-	private static final Logger LOGGER = Logger.getLogger(DelayPressureDB.class.getName());
 	private static final String FIELD = "Round(pressure,0)";
 	private static final String NAME = "rounded";
-
-	private static final Optional<DelayPressureData> getDelayLine(ResultSet result)
-	{
-		try
-		{
-			final DelayMaximum delayMaximum = new DelayMaximum(result.getDouble("delay_max"));
-			final DelayAverage delayAverage = new DelayAverage(result.getDouble("delay_avg"));
-			final double wind = result.getDouble(NAME);
-
-			return Optional.of(new DelayPressureData(delayMaximum, delayAverage, wind));
-		}
-		catch (SQLException e)
-		{
-			LOGGER.log(Level.WARNING, "Unable to parse to " + DelayPressureDB.class.getName(), e);
-			return Optional.empty();
-		}
-	}
 
 	public final List<DelayPressureData> getDelays() throws IOException
 	{
@@ -46,12 +27,22 @@ public class DelayPressureDB implements DelayPressure
 		try (PreparedStatement preparedStatement = database.getPreparedStatement(sql))
 		{
 			final List<DelayPressureData> list = new ArrayList<>();
-			database.select(r -> DelayPressureDB.getDelayLine(r).ifPresent(list::add), preparedStatement);
+			database.select(r -> parse(r).ifPresent(list::add), preparedStatement);
 			return list;
 		}
 		catch (SQLException e)
 		{
 			throw new IOException("Selecting does not succeed.", e);
 		}
+	}
+
+	@Override
+	protected Optional<DelayPressureData> getValue(ResultSet result) throws SQLException
+	{
+		final DelayMaximum delayMaximum = new DelayMaximum(result.getDouble("delay_max"));
+		final DelayAverage delayAverage = new DelayAverage(result.getDouble("delay_avg"));
+		final double wind = result.getDouble(NAME);
+
+		return Optional.of(new DelayPressureData(delayMaximum, delayAverage, wind));
 	}
 }
