db863529a1a0d8ce7d418e4b1683843bbdde9c42
ricokritzer <rico@kritzer.net>
2019-03-16 11:36:10 +0100

    Refactoring: Codeduplikate durch abstrakte Klasse

--- Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/reader/data/situation/SituationDB.java
+++ Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/reader/data/situation/SituationDB.java
@@ -1,50 +1,27 @@
 package de.dhbw.studienarbeit.data.reader.data.situation;
 
 import java.io.IOException;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-import de.dhbw.studienarbeit.data.reader.database.DatabaseReader;
+import de.dhbw.studienarbeit.data.reader.database.DB;
 
-public class SituationDB
+public class SituationDB extends DB<SituationData>
 {
-	private static final Logger LOGGER = Logger.getLogger(SituationDB.class.getName());
-
-	private final Optional<SituationData> getSituation(ResultSet result)
-	{
-		try
-		{
-			final SituationID id = new SituationID(result.getString("situationID"));
-			final SituationName name = new SituationName(result.getString("text"));
-
-			return Optional.of(new SituationData(name, id));
-		}
-		catch (SQLException e)
-		{
-			LOGGER.log(Level.WARNING, "Unable to parse to situation.", e);
-			return Optional.empty();
-		}
-	}
-
 	public final List<SituationData> getSituations() throws IOException
 	{
 		final String sql = "SELECT situationID, max(version), text FROM Situation GROUP BY situationID, text;";
-		final DatabaseReader database = new DatabaseReader();
-		try (PreparedStatement preparedStatement = database.getPreparedStatement(sql))
-		{
-			final List<SituationData> list = new ArrayList<>();
-			database.select(r -> getSituation(r).ifPresent(list::add), preparedStatement);
-			return list;
-		}
-		catch (SQLException e)
-		{
-			throw new IOException("Selecting does not succeed.", e);
-		}
+		return readFromDatabase(sql);
+	}
+
+	@Override
+	protected Optional<SituationData> getValue(ResultSet result) throws SQLException
+	{
+		final SituationID id = new SituationID(result.getString("situationID"));
+		final SituationName name = new SituationName(result.getString("text"));
+
+		return Optional.of(new SituationData(name, id));
 	}
 }
