16692ae5ce6bbce7def1263a241d74cffbdb179f
ricokritzer <rico@kritzer.net>
2019-03-16 11:37:33 +0100

    Refactoring: Codeduplikate

--- Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/reader/data/station/DelayStationDB.java
+++ Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/reader/data/station/DelayStationDB.java
@@ -1,43 +1,24 @@
 package de.dhbw.studienarbeit.data.reader.data.station;
 
 import java.io.IOException;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import de.dhbw.studienarbeit.data.reader.data.DelayAverage;
 import de.dhbw.studienarbeit.data.reader.data.DelayMaximum;
 import de.dhbw.studienarbeit.data.reader.database.DB;
-import de.dhbw.studienarbeit.data.reader.database.DatabaseReader;
 
 public class DelayStationDB extends DB<DelayStationData> implements DelayStation
 {
 	public final List<DelayStationData> getDelays() throws IOException
 	{
-		final String sql = "SELECT " //
-				+ "Stop.stationID, " //
-				+ "name, " //
-				+ "displayName, " //
-				+ "lat, " //
-				+ "lon, " //
-				+ "count(*) AS count, " //
+		final String sql = "SELECT Stop.stationID, name, displayName, lat, lon, count(*) AS count, "
 				+ "avg(UNIX_TIMESTAMP(realTime) - UNIX_TIMESTAMP(timeTabledTime)) AS delay_avg, "
 				+ "max(UNIX_TIMESTAMP(realTime) - UNIX_TIMESTAMP(timeTabledTime)) AS delay_max "
 				+ "FROM Stop, Station, Operator WHERE realTime IS NOT NULL AND Station.operator = Operator.operator AND Stop.stationID = Station.stationID GROUP BY Station.stationID ORDER BY delay_avg DESC;";
-		final DatabaseReader database = new DatabaseReader();
-		try (PreparedStatement preparedStatement = database.getPreparedStatement(sql))
-		{
-			final List<DelayStationData> list = new ArrayList<>();
-			database.select(r -> parse(r).ifPresent(list::add), preparedStatement);
-			return list;
-		}
-		catch (SQLException e)
-		{
-			throw new IOException("Selecting does not succeed.", e);
-		}
+		return readFromDatabase(sql);
 	}
 
 	@Override
