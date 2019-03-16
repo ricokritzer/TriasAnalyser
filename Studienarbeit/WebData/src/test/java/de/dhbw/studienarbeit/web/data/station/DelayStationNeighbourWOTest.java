76fdcb7abb0e697b59facdf21baeb3a2582bee1c
ricokritzer <rico@kritzer.net>
2019-03-16 13:45:55 +0100

    Deprecated Methoden ausgebaut

--- Studienarbeit/WebData/src/test/java/de/dhbw/studienarbeit/web/data/station/DelayStationNeighbourWOTest.java
+++ Studienarbeit/WebData/src/test/java/de/dhbw/studienarbeit/web/data/station/DelayStationNeighbourWOTest.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.hamcrest.core.Is;
 import org.junit.Test;
 
+import de.dhbw.studienarbeit.data.reader.data.DelayAverage;
 import de.dhbw.studienarbeit.data.reader.data.station.DelayStationNeighbourData;
 import de.dhbw.studienarbeit.data.reader.data.station.Position;
 import de.dhbw.studienarbeit.data.reader.data.station.StationName;
@@ -20,13 +21,13 @@ public class DelayStationNeighbourWOTest
 		final Position pos2 = new Position(1.0, 1.5);
 		final StationName name = new StationName("foo");
 
-		final double avg1Old = 10;
-		double avg2 = 20;
+		final DelayAverage avg1Old = new DelayAverage(10);
+		DelayAverage avg2 = new DelayAverage(20);
 		final DelayStationNeighbourData stationNeighbour = new DelayStationNeighbourData(name, pos1, avg1Old, name,
 				pos2, avg2);
 
-		final double avg1New = 15;
-		avg2 = 12;
+		final DelayAverage avg1New = new DelayAverage(15);
+		avg2 = new DelayAverage(12);
 		final DelayStationNeighbourData stationNeighbourDBLater = new DelayStationNeighbourData(name, pos1, avg1New,
 				name, pos2, avg2);
 
@@ -35,10 +36,10 @@ public class DelayStationNeighbourWOTest
 		assertThat(stationNeighbours.data.size(), Is.is(0));
 		stationNeighbours.renewData(stationNeighbour);
 		assertThat(stationNeighbours.data.size(), Is.is(1));
-		assertThat(stationNeighbours.data.get(0).getAvg1(), Is.is(avg1Old));
+		assertThat(stationNeighbours.data.get(0).getDelayAverage1(), Is.is(avg1Old));
 		stationNeighbours.renewData(stationNeighbourDBLater);
 		assertThat(stationNeighbours.data.size(), Is.is(1));
-		assertThat(stationNeighbours.data.get(0).getAvg1(), Is.is(avg1New));
+		assertThat(stationNeighbours.data.get(0).getDelayAverage1(), Is.is(avg1New));
 	}
 
 }
