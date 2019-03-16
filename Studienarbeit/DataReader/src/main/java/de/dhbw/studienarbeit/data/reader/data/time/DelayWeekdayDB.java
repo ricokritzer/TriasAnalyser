7a2ea7bb0e09da70af2e6e409cd1957148793d08
ricokritzer <rico@kritzer.net>
2019-03-16 11:47:35 +0100

    Refactoring: Codeduplikate

--- Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/reader/data/time/DelayWeekdayDB.java
+++ Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/reader/data/time/DelayWeekdayDB.java
@@ -1,34 +1,21 @@
 package de.dhbw.studienarbeit.data.reader.data.time;
 
 import java.io.IOException;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import de.dhbw.studienarbeit.data.reader.data.DelayAverage;
 import de.dhbw.studienarbeit.data.reader.data.DelayMaximum;
 import de.dhbw.studienarbeit.data.reader.database.DB;
-import de.dhbw.studienarbeit.data.reader.database.DatabaseReader;
 
 public class DelayWeekdayDB extends DB<DelayWeekdayData> implements DelayWeekday
 {
 	public final List<DelayWeekdayData> getDelays() throws IOException
 	{
 		final String sql = "SELECT WEEKDAY(timetabledTime) AS weekday, avg(UNIX_TIMESTAMP(realtime) - UNIX_TIMESTAMP(timetabledTime)) AS delay_avg, max(UNIX_TIMESTAMP(realtime) - UNIX_TIMESTAMP(timetabledTime)) AS delay_max FROM Stop GROUP BY weekday;";
-		final DatabaseReader database = new DatabaseReader();
-		try (PreparedStatement preparedStatement = database.getPreparedStatement(sql))
-		{
-			final List<DelayWeekdayData> list = new ArrayList<>();
-			database.select(r -> parse(r).ifPresent(list::add), preparedStatement);
-			return list;
-		}
-		catch (SQLException e)
-		{
-			throw new IOException("Selecting does not succeed.", e);
-		}
+		return readFromDatabase(sql);
 	}
 
 	@Override
