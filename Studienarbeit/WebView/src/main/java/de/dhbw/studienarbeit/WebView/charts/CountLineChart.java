b91e372914255b1ae09764b8b78c2adbd0dd16c1
Patrick <pat-sie@web.de>
2019-03-20 19:36:18 +0100

    Neue WelcomeView. Text + Anzeige von Charts bei Klick auf Text

---
+++ Studienarbeit/WebView/src/main/java/de/dhbw/studienarbeit/WebView/charts/CountLineChart.java
@@ -0,0 +1,73 @@
+package de.dhbw.studienarbeit.WebView.charts;
+
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.syndybat.chartjs.ChartJs;
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
+import be.ceau.chart.LineChart;
+import be.ceau.chart.color.Color;
+import be.ceau.chart.data.LineData;
+import be.ceau.chart.dataset.LineDataset;
+import be.ceau.chart.options.LineOptions;
+import de.dhbw.studienarbeit.data.reader.data.count.CountData;
+import de.dhbw.studienarbeit.web.data.counts.CountWO;
+
+public class CountLineChart extends Div
+{
+	private static final long serialVersionUID = 1L;
+	private List<CountWO> countWO;
+	private String name;
+
+	public CountLineChart(CountData current, List<CountWO> countWO, String name, String zusatz)
+	{
+		this.countWO = countWO;
+		this.name = name;
+		ChartJs chart = new ChartJs(getChart());
+		Div divChart = new Div(chart);
+		divChart.setVisible(false);
+		Div divText = new Div(new VerticalLayout(new H1(name), new H1(current.toString()), new Text(zusatz)));
+		add(divText, divChart);
+
+		divChart.addClickListener(event -> {
+			divChart.setVisible(false);
+			divText.setVisible(true);
+		});
+		
+		divText.addClickListener(event -> {
+			divText.setVisible(false);
+			divChart.setVisible(true);
+		});
+
+		setSizeFull();
+	}
+
+	protected String getChart()
+	{
+		LineDataset dataset = new LineDataset().setData(getData()).setLabel(name).setBorderColor(Color.BLUE)
+				.setBorderWidth(2).setBackgroundColor(Color.TRANSPARENT);
+
+		LineData data = new LineData().addDataset(dataset).addLabels(getLabels());
+
+		LineOptions options = new LineOptions().setResponsive(true);
+
+		return new LineChart(data, options).toJson();
+	}
+
+	private List<BigDecimal> getData()
+	{
+		return countWO.stream().map(e -> BigDecimal.valueOf(e.getValue().getValue())).collect(Collectors.toList());
+	}
+
+	private String[] getLabels()
+	{
+		return countWO.stream().map(e -> new SimpleDateFormat("HH:mm").format(e.getLastUpdate()))
+				.toArray(String[]::new);
+	}
+}
--- Studienarbeit/WebView/src/main/java/de/dhbw/studienarbeit/WebView/overview/WelcomeOverview.java
+++ Studienarbeit/WebView/src/main/java/de/dhbw/studienarbeit/WebView/overview/WelcomeOverview.java
@@ -1,15 +1,15 @@
 package de.dhbw.studienarbeit.WebView.overview;
 
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.router.Route;
 
-import de.dhbw.studienarbeit.WebView.charts.AbstractChart;
-import de.dhbw.studienarbeit.WebView.charts.LineCountChart;
-import de.dhbw.studienarbeit.WebView.charts.StationCountChart;
-import de.dhbw.studienarbeit.WebView.charts.StopAndWeatherCountChart;
+import de.dhbw.studienarbeit.WebView.charts.CountLineChart;
+import de.dhbw.studienarbeit.web.data.Data;
 
 @Route("")
 public class WelcomeOverview extends Overview
@@ -18,51 +18,34 @@ public class WelcomeOverview extends Overview
 
 	public WelcomeOverview()
 	{
-		super();
+		VerticalLayout txtUnsereStudienarbeit = new VerticalLayout(new H1("Unsere Studienarbeit"),
+				new Div(new Text("Patrick Siewert")), new Div(new Text("Rico Kritzer")));
+		txtUnsereStudienarbeit.setAlignItems(Alignment.STRETCH);
 
-		VerticalLayout textAreas = new VerticalLayout();
-		textAreas.setAlignItems(Alignment.CENTER);
-		textAreas.setWidth("25%");
-		
-		TextArea txtAbout = new TextArea();
-		txtAbout.setLabel("Unsere Studienarbeit");
-		txtAbout.setReadOnly(true);
-		txtAbout.setValue("Patrick Siewert" + System.lineSeparator() + "Rico Kritzer");
-		txtAbout.setSizeFull();
-		textAreas.add(txtAbout);
+		VerticalLayout txtImpressum = new VerticalLayout(new H1("Impressum"), new Div(new Text("Duale Hochschule")),
+				new Div(new Text("Erzbergerstraße 121")), new Div(new Text("76133 Karlsruhe")),
+				new Div(new Text("Deutschland")));
+		txtImpressum.setAlignItems(Alignment.STRETCH);
 
-		final TextArea txtImpress = new TextArea();
-		txtImpress.setLabel("Impressum");
-		txtImpress.setReadOnly(true);
-		txtImpress.setValue(new StringBuilder() //
-				.append("Duale Hochschule").append(System.lineSeparator()) //
-				.append("Erzbergerstraße 121").append(System.lineSeparator()) //
-				.append("76133 Karlsruhe").append(System.lineSeparator()) //
-				.append("Deutschland") //
-				.toString());
-		txtImpress.setSizeFull();
-		textAreas.add(txtImpress);
-		
-		AbstractChart stationChart = new StationCountChart();
-		stationChart.setWidth("50%");
-		AbstractChart lineChart = new LineCountChart();
-		lineChart.setWidth("50%");
-		
-		HorizontalLayout smallerCharts = new HorizontalLayout(stationChart, lineChart);
-		smallerCharts.setAlignItems(Alignment.CENTER);
-		smallerCharts.setHeight("50%");
-		
-		AbstractChart stopWeatherChart = new StopAndWeatherCountChart();
-		stopWeatherChart.setWidth("50%");
-		
-		VerticalLayout charts = new VerticalLayout(stopWeatherChart, smallerCharts);
-		charts.setAlignItems(Alignment.STRETCH);
-		charts.setWidth("75%");
+		HorizontalLayout row1 = new HorizontalLayout(txtUnsereStudienarbeit,
+				new CountLineChart(Data.getCountStopsLatest(), Data.getCountStops(), "Stops", "gespeichert"),
+				new CountLineChart(Data.getCountWeathersLatest(), Data.getCountWeathers(), "Wettereinträge",
+						"gespeichert"));
+		row1.setSizeFull();
+		row1.setAlignItems(Alignment.STRETCH);
 
-		HorizontalLayout outer = new HorizontalLayout(textAreas, charts);
-		outer.setAlignItems(Alignment.STRETCH);
-		outer.setSizeFull();
-		
-		setContent(outer);
+		HorizontalLayout row2 = new HorizontalLayout(txtImpressum,
+				new CountLineChart(Data.getCountLinesLatest(), Data.getCountLines(), "Linien", "gespeichert"),
+				new CountLineChart(Data.getCountObservedStationsLatest(), Data.getCountObservedStations(),
+						"Haltestellen", "unter Beobachtung"));
+		row2.setSizeFull();
+		row2.setAlignItems(Alignment.STRETCH);
+
+		VerticalLayout content = new VerticalLayout(row1, row2);
+		content.setMargin(false);
+		content.setPadding(false);
+		content.setSizeFull();
+
+		setContent(content);
 	}
 }
