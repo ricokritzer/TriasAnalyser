dc2a48ab03e07cab8336ab989280c96e3ca40eb1
ricokritzer <rico@kritzer.net>
2019-03-22 08:40:00 +0100

    Hour eingefügt (enum)

---
+++ Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/reader/data/time/Hour.java
@@ -0,0 +1,59 @@
+package de.dhbw.studienarbeit.data.reader.data.time;
+
+public enum Hour
+{
+	HOUR1(1), //
+	HOUR2(2), //
+	HOUR3(3), //
+	HOUR4(4), //
+	HOUR5(5), //
+	HOUR6(6), //
+	HOUR7(7), //
+	HOUR8(8), //
+	HOUR9(9), //
+	HOUR10(10), //
+	HOUR11(11), //
+	HOUR12(12), //
+	HOUR13(13), //
+	HOUR14(14), //
+	HOUR15(15), //
+	HOUR16(16), //
+	HOUR17(17), //
+	HOUR18(18), //
+	HOUR19(19), //
+	HOUR20(20), //
+	HOUR21(21), //
+	HOUR22(22), //
+	HOUR23(23), //
+	HOUR24(24), //
+
+	;
+
+	private final int value;
+
+	private Hour(int value)
+	{
+		if (value < 0)
+		{
+			throw new IllegalArgumentException("Hour could not be negative.");
+		}
+
+		if (value > 24)
+		{
+			throw new IllegalArgumentException("Hour could not be over 24.");
+		}
+
+		this.value = value;
+	}
+
+	public int getValue()
+	{
+		return value;
+	}
+
+	@Override
+	public String toString()
+	{
+		return new StringBuilder(value).append(":00 Uhr").toString();
+	}
+}
