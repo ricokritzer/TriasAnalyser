c1b23c962e1c5c14f89bf14d1713e0315177986d
Patrick <pat-sie@web.de>
2019-03-24 12:05:03 +0100

    BarChart für Verspätungen bei eigenen Analysen

--- Studienarbeit/WebView/src/main/java/de/dhbw/studienarbeit/WebView/overview/AnalyseOverview.java
+++ Studienarbeit/WebView/src/main/java/de/dhbw/studienarbeit/WebView/overview/AnalyseOverview.java
@@ -2,17 +2,31 @@ package de.dhbw.studienarbeit.WebView.overview;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 
+import com.syndybat.chartjs.ChartJs;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
+import be.ceau.chart.BarChart;
+import be.ceau.chart.color.Color;
+import be.ceau.chart.data.BarData;
+import be.ceau.chart.dataset.BarDataset;
+import be.ceau.chart.options.BarOptions;
+import be.ceau.chart.options.scales.BarScale;
+import be.ceau.chart.options.scales.YAxis;
+import be.ceau.chart.options.ticks.LinearTicks;
 import de.dhbw.studienarbeit.data.reader.data.Delay;
 import de.dhbw.studienarbeit.data.reader.data.DelayAverage;
 import de.dhbw.studienarbeit.data.reader.data.DelayMaximum;
@@ -47,6 +61,8 @@ public class AnalyseOverview extends Overview
 
 	private boolean updating = false;
 
+	private VerticalLayout layout;
+
 	public AnalyseOverview()
 	{
 		super();
@@ -74,7 +90,7 @@ public class AnalyseOverview extends Overview
 		filters.add(end);
 
 		filters.forEach(e -> e.setEnabled(false));
-
+		
 		stations = new ComboBox<>("Station",
 				new DelayStationData(new DelayMaximum(10), new DelayAverage(5), new StationID("de:08212:1"),
 						new StationName("test"), new OperatorName("test"), new Position(10, 10), 10));
@@ -87,9 +103,9 @@ public class AnalyseOverview extends Overview
 		filters.forEach(filter::add);
 		filter.add(btnSearch);
 
-		VerticalLayout layout = new VerticalLayout(filter);
+		layout = new VerticalLayout(filter);
 		layout.setSizeFull();
-		layout.setAlignItems(Alignment.START);
+		layout.setAlignItems(Alignment.STRETCH);
 
 		setContent(layout);
 	}
@@ -109,7 +125,71 @@ public class AnalyseOverview extends Overview
 
 	private void showDelays()
 	{
-		delays.forEach(System.out::println);
+		Collections.sort(delays);
+		
+		Map<Delay, Integer> delayCounts = new HashMap<>();
+		Div div = new Div();
+		
+		for (Delay delay : delays)
+		{
+			if (delayCounts.containsKey(delay))
+			{
+				delayCounts.put(delay, delayCounts.get(delay) + 1);
+			}
+			else
+			{
+				delayCounts.put(delay, 1);
+			}
+		}
+		
+		List<Delay> sortedKeySet = asSortedList(delayCounts.keySet());
+		
+		BarDataset dataset = new BarDataset().setData(sortedData(sortedKeySet, delayCounts)).setLabel("Verspätungen").setBackgroundColor(Color.BLUE);
+		
+		BarData data = new BarData().addLabels(asSortedArray(delayCounts.keySet())).addDataset(dataset);
+		
+		BarOptions options = new BarOptions().setScales(new BarScale().setyAxes(getYAxis()));
+		
+		BarChart chart = new BarChart().setData(data).setOptions(options);
+		
+		ChartJs chartJS = new ChartJs(chart.toJson());
+		
+		div.add(chartJS);
+		
+		layout.add(chartJS);
+	}
+
+	private List<YAxis<LinearTicks>> getYAxis()
+	{
+		List<YAxis<LinearTicks>> axis = new ArrayList<>();
+		YAxis<LinearTicks> ticks = new YAxis<>();
+		ticks.setType("logarithmic");
+		axis.add(ticks);
+		return axis;
+	}
+
+	private int[] sortedData(List<Delay> sortedKeySet, Map<Delay, Integer> delayCounts)
+	{
+		int[] data = new int[sortedKeySet.size()];
+		int i = 0;
+		for (Delay delay : sortedKeySet)
+		{
+			data[i] = delayCounts.get(delay);
+			i++;
+		}
+		return data;
+	}
+
+	private List<Delay> asSortedList(Set<Delay> keySet)
+	{
+		List<Delay> list = new ArrayList<>(keySet);
+		Collections.sort(list);
+		return list;
+	}
+
+	private String[] asSortedArray(Set<Delay> keySet)
+	{
+		return asSortedList(keySet).stream().map(e -> e.toString()).toArray(String[]::new);
 	}
 
 	private void setHourEnd()
