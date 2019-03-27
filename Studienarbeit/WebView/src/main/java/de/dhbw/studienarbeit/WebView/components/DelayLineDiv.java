0583c5c78a75726649b7dd57fb581d8cb6217e3b
ricokritzer <rico@kritzer.net>
2019-03-27 10:13:49 +0100

    Fehlerbehebung: LastUpdated korrigiert + Deprecated Methoden umgebaut.

--- Studienarbeit/WebView/src/main/java/de/dhbw/studienarbeit/WebView/components/DelayLineDiv.java
+++ Studienarbeit/WebView/src/main/java/de/dhbw/studienarbeit/WebView/components/DelayLineDiv.java
@@ -30,23 +30,23 @@ public class DelayLineDiv extends Div
 
 		field.setLabel("Stand");
 		field.setReadOnly(true);
-		field.setValue(new SimpleDateFormat("dd.MM.yyyy HH:mm:ss").format(Data.getDelayLineWO().getLastUpdated()));
+		field.setValue(new SimpleDateFormat("dd.MM.yyyy HH:mm:ss").format(Data.getDelaysLineLastUpdated()));
 		layout.add(field);
 
-		grid.addColumn(db -> db.getLineName()).setHeader("Linie").setSortable(true);
-		grid.addColumn(db -> db.getLineDestination()).setHeader("Ziel").setSortable(true);
+		grid.addColumn(db -> db.getValue().getName().toString()).setHeader("Linie").setSortable(true);
+		grid.addColumn(db -> db.getValue().getDestination().toString()).setHeader("Ziel").setSortable(true);
 		grid.addColumn(db -> db.getAverage().toString()).setHeader("Durchschnitt")
 				.setComparator((db1, db2) -> db1.getAverage().compareTo(db2.getAverage())).setSortable(true);
 		grid.addColumn(db -> db.getMaximum().toString()).setHeader("Maximum")
 				.setComparator((db1, db2) -> db1.getMaximum().compareTo(db2.getMaximum())).setSortable(true);
 
-		grid.setDataProvider(DataProvider.ofCollection(Data.getDelayLineWO().getData()));
+		grid.setDataProvider(DataProvider.ofCollection(Data.getDelaysLine()));
 
 		grid.setHeight("70vh");
 		grid.setSelectionMode(SelectionMode.NONE);
 
 		layout.add(grid);
-		
+
 		add(layout);
 	}
 }
