e8bcd67cc522ea6ce5d9a5b4c1d1e05539694278
ricokritzer <rico@kritzer.net>
2019-03-29 16:02:23 +0100

    Texte hinzugefügt.

--- Studienarbeit/WebView/src/main/java/de/dhbw/studienarbeit/WebView/tiles/CountView.java
+++ Studienarbeit/WebView/src/main/java/de/dhbw/studienarbeit/WebView/tiles/CountView.java
@@ -1,16 +1,21 @@
 package de.dhbw.studienarbeit.WebView.tiles;
 
-import com.vaadin.flow.component.Component;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 
 import de.dhbw.studienarbeit.data.reader.data.count.CountData;
 
-public class CountView extends Component
+public class CountView extends Div
 {
+	private static final long serialVersionUID = 1L;
+
 	public CountView(String what, CountData count)
 	{
-		// add(createBorderlessHeadline(count.toString()));
-		// add(createBorderlessHeadline(what));
+		final VerticalLayout verticalLayout = new VerticalLayout();
+		verticalLayout.add(createBorderlessHeadline(count.toString()));
+		verticalLayout.add(createBorderlessHeadline(what));
+		add(verticalLayout);
 	}
 
 	private H1 createBorderlessHeadline(String string)
