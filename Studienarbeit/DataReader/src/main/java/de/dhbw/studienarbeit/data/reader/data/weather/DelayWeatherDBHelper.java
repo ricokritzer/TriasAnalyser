50ba7ec3e680e0543e8acffc79978e938730c870
ricokritzer <rico@kritzer.net>
2019-04-02 10:58:40 +0200

    Sortierung nach Value statt Delay

--- Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/reader/data/weather/DelayWeatherDBHelper.java
+++ Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/reader/data/weather/DelayWeatherDBHelper.java
@@ -12,6 +12,6 @@ public class DelayWeatherDBHelper
 				.append("max(UNIX_TIMESTAMP(Stop.realTime) - UNIX_TIMESTAMP(Stop.timeTabledTime)) AS delay_max, ")
 				.append(what).append(" AS ").append(name).append(" FROM StopWeather, Stop, Weather ")
 				.append("WHERE Stop.stopID = StopWeather.stopID AND StopWeather.weatherId = Weather.id ")
-				.append("GROUP BY ").append(name).append(" ORDER BY delay_avg DESC;").toString();
+				.append("GROUP BY ").append(name).append(" ORDER BY ").append(name).append(";").toString();
 	}
 }
