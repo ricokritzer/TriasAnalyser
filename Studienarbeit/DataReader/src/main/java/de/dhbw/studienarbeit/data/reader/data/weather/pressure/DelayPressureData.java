08827330e86fede0a3495d50648735caa4428f58
ricokritzer <rico@kritzer.net>
2019-04-03 16:09:59 +0200

    Fehlerbehebung: Datentypen auf Double zurückgeändert

--- Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/reader/data/weather/pressure/DelayPressureData.java
+++ Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/reader/data/weather/pressure/DelayPressureData.java
@@ -4,9 +4,9 @@ import de.dhbw.studienarbeit.data.reader.data.DelayAverage;
 import de.dhbw.studienarbeit.data.reader.data.DelayData;
 import de.dhbw.studienarbeit.data.reader.data.DelayMaximum;
 
-public class DelayPressureData extends DelayData<Integer>
+public class DelayPressureData extends DelayData<Double>
 {
-	public DelayPressureData(DelayMaximum delayMaximum, DelayAverage delayAverage, int pressure)
+	public DelayPressureData(DelayMaximum delayMaximum, DelayAverage delayAverage, double pressure)
 	{
 		super(delayMaximum, delayAverage, pressure);
 	}
--- Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/reader/data/weather/temperature/DelayTemperatureData.java
+++ Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/reader/data/weather/temperature/DelayTemperatureData.java
@@ -4,9 +4,9 @@ import de.dhbw.studienarbeit.data.reader.data.DelayAverage;
 import de.dhbw.studienarbeit.data.reader.data.DelayData;
 import de.dhbw.studienarbeit.data.reader.data.DelayMaximum;
 
-public class DelayTemperatureData extends DelayData<Integer>
+public class DelayTemperatureData extends DelayData<Double>
 {
-	public DelayTemperatureData(DelayMaximum delayMaximum, DelayAverage delayAverage, int temperature)
+	public DelayTemperatureData(DelayMaximum delayMaximum, DelayAverage delayAverage, double temperature)
 	{
 		super(delayMaximum, delayAverage, temperature);
 	}
