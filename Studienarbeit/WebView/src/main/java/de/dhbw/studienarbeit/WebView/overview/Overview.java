e1c1e6f20272aa40773a210a65ebb99954766cd3
Patrick <pat-sie@web.de>
2019-04-04 17:44:05 +0200

    neue Icons für Menü

--- Studienarbeit/WebView/src/main/java/de/dhbw/studienarbeit/WebView/overview/Overview.java
+++ Studienarbeit/WebView/src/main/java/de/dhbw/studienarbeit/WebView/overview/Overview.java
@@ -17,9 +17,9 @@ public abstract class Overview extends AppLayout
 		AppLayoutMenuItem itemMap = new AppLayoutMenuItem(VaadinIcon.GLOBE.create(), "Karte", "map");
 		AppLayoutMenuItem itemWeather = new AppLayoutMenuItem(VaadinIcon.CLOUD.create(), "Wetter", "weather");
 		AppLayoutMenuItem itemTime = new AppLayoutMenuItem(VaadinIcon.CLOCK.create(), "Zeit", "time");
-		AppLayoutMenuItem itemDelay = new AppLayoutMenuItem(VaadinIcon.CLOCK.create(), "Verspätung", "delay");
-		AppLayoutMenuItem itemAnalyse = new AppLayoutMenuItem(VaadinIcon.CLOCK.create(), "Eigene Analysen", "analyse");
-		AppLayoutMenuItem itemAnalyseFixedTime = new AppLayoutMenuItem(VaadinIcon.CLOCK.create(), "Eigene Analysen (fester Zeitraum)", "analyseFixedTime");
+		AppLayoutMenuItem itemDelay = new AppLayoutMenuItem(VaadinIcon.TIMER.create(), "Verspätung", "delay");
+		AppLayoutMenuItem itemAnalyse = new AppLayoutMenuItem(VaadinIcon.CHART.create(), "Eigene Analysen", "analyse");
+		AppLayoutMenuItem itemAnalyseFixedTime = new AppLayoutMenuItem(VaadinIcon.CHART.create(), "Eigene Analysen (fester Zeitraum)", "analyseFixedTime");
 		
 		AppLayoutMenu menu = new AppLayoutMenu();
 		menu.addMenuItems(itemWelcome, itemMap, itemWeather, itemTime, itemDelay, itemAnalyse, itemAnalyseFixedTime);
