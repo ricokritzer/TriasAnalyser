d05a810fcce6a6cd39f93e09566b72e5662516fd
ricokritzer <rico@kritzer.net>
2019-04-05 16:35:54 +0200

    Implementierung von RequestDB

---
+++ Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/reader/data/request/RequestDB.java
@@ -0,0 +1,266 @@
+package de.dhbw.studienarbeit.data.reader.data.request;
+
+import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import de.dhbw.studienarbeit.data.reader.data.Delay;
+import de.dhbw.studienarbeit.data.reader.data.count.CountData;
+import de.dhbw.studienarbeit.data.reader.data.line.LineDestination;
+import de.dhbw.studienarbeit.data.reader.data.line.LineName;
+import de.dhbw.studienarbeit.data.reader.data.station.StationID;
+import de.dhbw.studienarbeit.data.reader.data.time.Hour;
+import de.dhbw.studienarbeit.data.reader.data.time.Weekday;
+import de.dhbw.studienarbeit.data.reader.database.DB;
+import de.dhbw.studienarbeit.data.reader.database.SQLListHelper;
+
+public class RequestDB extends DB<DelayCountData> implements Request
+{
+	private final StationID stationID;
+
+	private List<LineName> lineNames = new ArrayList<>();
+	private List<LineDestination> lineDestinations = new ArrayList<>();
+
+	private Optional<Date> startDate = Optional.empty();
+	private Optional<Date> endDate = Optional.empty();
+
+	private Optional<Hour> hourStart = Optional.empty();
+	private Optional<Hour> hourEnd = Optional.empty();
+
+	private List<Weekday> weekdays = new ArrayList<>();
+
+	public RequestDB(StationID stationID)
+	{
+		this.stationID = stationID;
+	}
+
+	@Override
+	public CountData getCancelledStops() throws IOException
+	{
+		final List<DelayCountData> data = readFromDatabase(getCountCancelledSQL(), this::setValues);
+
+		if (data.isEmpty())
+		{
+			return CountData.UNABLE_TO_COUNT;
+		}
+
+		return data.get(0).getCount();
+	}
+
+	@Override
+	public List<DelayCountData> getDelays() throws IOException
+	{
+		return readFromDatabase(getDelaySQL(), this::setValues);
+	}
+
+	@Override
+	public List<LineName> getPossibleLineNames() throws IOException
+	{
+		return new LineNamesAtStationDB().getLineNamesAt(stationID, lineDestinations);
+	}
+
+	@Override
+	public List<LineDestination> getPossibleLineDestinations() throws IOException
+	{
+		return new LineDestinationsAtStationDB().getLineDestinationsAt(stationID, lineNames);
+	}
+
+	@Override
+	public Request filterLineNames(Collection<LineName> lineNames)
+	{
+		this.lineNames.clear();
+		this.lineNames.addAll(lineNames);
+		return this;
+	}
+
+	@Override
+	public Request filterLineDestination(Collection<LineDestination> lineDestinations)
+	{
+		this.lineDestinations.clear();
+		this.lineDestinations.addAll(lineDestinations);
+		return this;
+	}
+
+	@Override
+	public Request filterWeekdays(Collection<Weekday> weekdays)
+	{
+		this.weekdays.clear();
+		this.weekdays.addAll(weekdays);
+		return this;
+	}
+
+	@Override
+	public Request filterStartDate(Date start)
+	{
+		this.startDate = Optional.ofNullable(start);
+		return this;
+	}
+
+	@Override
+	public Request filterEndDate(Date end)
+	{
+		this.endDate = Optional.ofNullable(end);
+		return this;
+	}
+
+	@Override
+	public Request filterStartHour(Hour hour) throws InvalidTimeSpanException
+	{
+		final Optional<Hour> setted = Optional.ofNullable(hour);
+
+		if (possibleHour(setted, hourEnd))
+		{
+			this.hourStart = setted;
+			return this;
+		}
+		else
+		{
+			throw new InvalidTimeSpanException();
+		}
+	}
+
+	@Override
+	public Request filterEndHour(Hour hour) throws InvalidTimeSpanException
+	{
+		final Optional<Hour> setted = Optional.ofNullable(hour);
+
+		if (possibleHour(hourStart, setted))
+		{
+			this.hourEnd = setted;
+			return this;
+		}
+		else
+		{
+			throw new InvalidTimeSpanException();
+		}
+	}
+
+	protected String getDelaySQL()
+	{
+		return getSQL(false);
+	}
+
+	protected String getCountCancelledSQL()
+	{
+		return getSQL(true);
+	}
+
+	private String getSQL(boolean realtimeNull)
+	{
+		final StringBuilder stringBuilder = new StringBuilder().append("SELECT ")
+				.append("count(*) AS total, (UNIX_TIMESTAMP(realtime) - UNIX_TIMESTAMP(timetabledTime)) AS delay")
+				.append(" FROM Stop WHERE stationID = ?");
+
+		startDate.ifPresent(h -> stringBuilder.append(" AND timetabledTime >= ?"));
+		endDate.ifPresent(h -> stringBuilder.append(" AND timetabledTime <= ?"));
+
+		stringBuilder.append(SQLListHelper.createSQLFor(" AND name", lineNames));
+		stringBuilder.append(SQLListHelper.createSQLFor(" AND destination", lineDestinations));
+		stringBuilder.append(SQLListHelper.createSQLFor(" AND WEEKDAY(timetabledTime)", weekdays));
+
+		hourStart.ifPresent(h -> stringBuilder.append(" AND HOUR(timetabledTime) >= ?"));
+		hourEnd.ifPresent(h -> stringBuilder.append(" AND HOUR(timetabledTime) <= ?"));
+
+		return stringBuilder.append(realtimeNull ? " AND realtime IS NULL" : " AND realtime IS NOT NULL")
+				.append(" GROUP BY delay;").toString();
+	}
+
+	private void setValues(PreparedStatement preparedStatement) throws SQLException
+	{
+		int idx = 1;
+		preparedStatement.setString(idx++, stationID.getValue());
+
+		if (startDate.isPresent())
+		{
+			preparedStatement.setTimestamp(idx, new Timestamp(startDate.get().getTime()));
+			idx++;
+		}
+
+		if (endDate.isPresent())
+		{
+			preparedStatement.setTimestamp(idx, new Timestamp(endDate.get().getTime()));
+			idx++;
+		}
+
+		for (LineName lineName : lineNames)
+		{
+			preparedStatement.setString(idx, lineName.toString());
+			idx++;
+		}
+
+		for (LineDestination lineDestination : lineDestinations)
+		{
+			preparedStatement.setString(idx, lineDestination.toString());
+			idx++;
+		}
+
+		for (Weekday weekday : weekdays)
+		{
+			preparedStatement.setInt(idx, weekday.getIdx());
+			idx++;
+		}
+
+		if (hourStart.isPresent())
+		{
+			preparedStatement.setInt(idx, hourStart.get().getValue());
+			idx++;
+		}
+
+		if (hourEnd.isPresent())
+		{
+			preparedStatement.setInt(idx, hourEnd.get().getValue());
+		}
+	}
+
+	@Override
+	protected Optional<DelayCountData> getValue(ResultSet result) throws SQLException
+	{
+		final Delay delay = new Delay(result.getDouble("delay"));
+		final CountData count = new CountData(result.getLong("total"));
+
+		return Optional.of(new DelayCountData(delay, count));
+	}
+
+	private boolean possibleDate(Optional<Date> start, Optional<Date> end)
+	{
+		if (!start.isPresent())
+		{
+			return true;
+		}
+
+		if (!end.isPresent())
+		{
+			return true;
+		}
+
+		final Date s = start.get();
+		final Date e = end.get();
+
+		return !e.before(s);
+	}
+
+	private boolean possibleHour(Optional<Hour> start, Optional<Hour> end)
+	{
+		if (!start.isPresent())
+		{
+			return true;
+		}
+
+		if (!end.isPresent())
+		{
+			return true;
+		}
+
+		final Hour s = start.get();
+		final Hour e = end.get();
+
+		return !e.before(s);
+	}
+}
---
+++ Studienarbeit/DataReader/src/test/java/de/dhbw/studienarbeit/data/reader/data/request/RequestDBTest.java
@@ -0,0 +1,221 @@
+package de.dhbw.studienarbeit.data.reader.data.request;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.junit.Test;
+
+import de.dhbw.studienarbeit.data.reader.data.line.LineDestination;
+import de.dhbw.studienarbeit.data.reader.data.line.LineName;
+import de.dhbw.studienarbeit.data.reader.data.station.StationID;
+import de.dhbw.studienarbeit.data.reader.data.time.Hour;
+import de.dhbw.studienarbeit.data.reader.data.time.Weekday;
+
+public class RequestDBTest
+{
+	@Test
+	public void sqlStationNameOnly() throws Exception
+	{
+		final RequestDB request = createDelayRequest();
+		final String sql = "SELECT count(*) AS total, (UNIX_TIMESTAMP(realtime) - UNIX_TIMESTAMP(timetabledTime)) AS delay "
+				+ "FROM Stop WHERE stationID = ? AND realtime IS NOT NULL GROUP BY delay;";
+
+		assertThat(request.getDelaySQL(), is(sql));
+	}
+
+	@Test
+	public void sqlStationNameAndWeekday() throws Exception
+	{
+		final RequestDB request = createDelayRequest();
+		request.filterWeekdays(createWeekdayList(1));
+
+		final String sql = "SELECT count(*) AS total, (UNIX_TIMESTAMP(realtime) - UNIX_TIMESTAMP(timetabledTime)) AS delay "
+				+ "FROM Stop WHERE stationID = ?"
+				+ " AND WEEKDAY(timetabledTime) IN (?) AND realtime IS NOT NULL GROUP BY delay;";
+
+		assertThat(request.getDelaySQL(), is(sql));
+	}
+
+	@Test
+	public void sqlStationNameAndMultipleWeekdays() throws Exception
+	{
+		final RequestDB request = createDelayRequest();
+		request.filterWeekdays(createWeekdayList(2));
+
+		final String sql = "SELECT count(*) AS total, (UNIX_TIMESTAMP(realtime) - UNIX_TIMESTAMP(timetabledTime)) AS delay "
+				+ "FROM Stop WHERE stationID = ?"
+				+ " AND WEEKDAY(timetabledTime) IN (?, ?) AND realtime IS NOT NULL GROUP BY delay;";
+
+		assertThat(request.getDelaySQL(), is(sql));
+	}
+
+	@Test
+	public void sqlLineNames() throws Exception
+	{
+		final RequestDB request = createDelayRequest();
+		request.filterLineNames(createLineNameList(1));
+
+		final String sql = "SELECT count(*) AS total, (UNIX_TIMESTAMP(realtime) - UNIX_TIMESTAMP(timetabledTime)) AS delay "
+				+ "FROM Stop WHERE stationID = ? AND name IN (?) AND realtime IS NOT NULL GROUP BY delay;";
+
+		assertThat(request.getDelaySQL(), is(sql));
+	}
+
+	@Test
+	public void sqlMultipleLineNames() throws Exception
+	{
+		final RequestDB request = createDelayRequest();
+		request.filterLineNames(createLineNameList(2));
+
+		final String sql = "SELECT count(*) AS total, (UNIX_TIMESTAMP(realtime) - UNIX_TIMESTAMP(timetabledTime)) AS delay "
+				+ "FROM Stop WHERE stationID = ? AND name IN (?, ?) AND realtime IS NOT NULL GROUP BY delay;";
+
+		assertThat(request.getDelaySQL(), is(sql));
+	}
+
+	@Test
+	public void sqlLineNamesAndDestinations() throws Exception
+	{
+		final RequestDB request = createDelayRequest();
+		request.filterLineNames(createLineNameList(2));
+		request.filterLineDestination(createLineDestinationList(2));
+
+		final String sql = "SELECT count(*) AS total, (UNIX_TIMESTAMP(realtime) - UNIX_TIMESTAMP(timetabledTime)) AS delay "
+				+ "FROM Stop WHERE stationID = ? AND name IN (?, ?) AND destination IN (?, ?) AND realtime IS NOT NULL GROUP BY delay;";
+
+		assertThat(request.getDelaySQL(), is(sql));
+	}
+
+	@Test
+	public void sqlCount() throws Exception
+	{
+		final RequestDB request = createDelayRequest();
+		request.filterLineNames(createLineNameList(1));
+
+		final String sql = "SELECT count(*) AS total, (UNIX_TIMESTAMP(realtime) - UNIX_TIMESTAMP(timetabledTime)) AS delay FROM Stop WHERE stationID = ? AND name IN (?) AND realtime IS NULL GROUP BY delay;";
+
+		assertThat(request.getCountCancelledSQL(), is(sql));
+	}
+
+	@Test
+	public void setStartTimeWithoutEnd() throws Exception
+	{
+		final RequestDB request = createDelayRequest();
+		request.filterStartHour(Hour.HOUR10);
+	}
+
+	@Test
+	public void setEndTimeWithoutStart() throws Exception
+	{
+		final RequestDB request = createDelayRequest();
+		request.filterEndHour(Hour.HOUR10);
+	}
+
+	@Test(expected = InvalidTimeSpanException.class)
+	public void setStartThanEndAndEndtimeBeforeStarttime() throws Exception
+	{
+		final RequestDB request = createDelayRequest();
+		request.filterEndHour(Hour.HOUR1);
+		request.filterStartHour(Hour.HOUR10);
+	}
+
+	@Test(expected = InvalidTimeSpanException.class)
+	public void setEndThanStartAndEndtimeBeforeStarttime() throws Exception
+	{
+		final RequestDB request = createDelayRequest();
+		request.filterStartHour(Hour.HOUR10);
+		request.filterEndHour(Hour.HOUR1);
+	}
+
+	@Test
+	public void setStartAndEndTimeSameValue() throws Exception
+	{
+		final RequestDB request = createDelayRequest();
+		request.filterStartHour(Hour.HOUR10);
+		request.filterEndHour(Hour.HOUR10);
+	}
+
+	@Test
+	public void setValidStartAndEndTime() throws Exception
+	{
+		final RequestDB request = createDelayRequest();
+		request.filterStartHour(Hour.HOUR1);
+		request.filterEndHour(Hour.HOUR10);
+	}
+
+	@Test
+	public void sqlWithStartTime() throws Exception
+	{
+		final RequestDB request = createDelayRequest();
+		request.filterStartHour(Hour.HOUR1);
+
+		final String sql = "SELECT count(*) AS total, (UNIX_TIMESTAMP(realtime) - UNIX_TIMESTAMP(timetabledTime)) AS delay "
+				+ "FROM Stop WHERE stationID = ? AND HOUR(timetabledTime) >= ? AND realtime IS NOT NULL GROUP BY delay;";
+
+		assertThat(request.getDelaySQL(), is(sql));
+	}
+
+	@Test
+	public void sqlWithEndTime() throws Exception
+	{
+		final RequestDB request = createDelayRequest();
+		request.filterEndHour(Hour.HOUR1);
+
+		final String sql = "SELECT count(*) AS total, (UNIX_TIMESTAMP(realtime) - UNIX_TIMESTAMP(timetabledTime)) AS delay "
+				+ "FROM Stop WHERE stationID = ? AND HOUR(timetabledTime) <= ? AND realtime IS NOT NULL GROUP BY delay;";
+
+		assertThat(request.getDelaySQL(), is(sql));
+	}
+
+	@Test
+	public void sqlWithStartAndEndTime() throws Exception
+	{
+		final RequestDB request = createDelayRequest();
+		request.filterStartHour(Hour.HOUR1);
+		request.filterEndHour(Hour.HOUR10);
+
+		final String sql = "SELECT count(*) AS total, (UNIX_TIMESTAMP(realtime) - UNIX_TIMESTAMP(timetabledTime)) AS delay "
+				+ "FROM Stop WHERE stationID = ? AND HOUR(timetabledTime) >= ? AND HOUR(timetabledTime) <= ? AND realtime IS NOT NULL GROUP BY delay;";
+
+		assertThat(request.getDelaySQL(), is(sql));
+	}
+
+	private RequestDB createDelayRequest()
+	{
+		return new RequestDB(new StationID("myStationName"));
+	}
+
+	private List<LineName> createLineNameList(int count)
+	{
+		final List<LineName> names = new ArrayList<>();
+		for (int i = 0; i < count; i++)
+		{
+			names.add(new LineName("foo"));
+		}
+		return names;
+	}
+
+	private List<LineDestination> createLineDestinationList(int count)
+	{
+		final List<LineDestination> destinations = new ArrayList<>();
+		for (int i = 0; i < count; i++)
+		{
+			destinations.add(new LineDestination("foo"));
+		}
+		return destinations;
+	}
+
+	private Collection<Weekday> createWeekdayList(int count)
+	{
+		final List<Weekday> weekdays = new ArrayList<>();
+		for (int i = 0; i < count; i++)
+		{
+			weekdays.add(Weekday.MONDAY);
+		}
+		return weekdays;
+	}
+}
