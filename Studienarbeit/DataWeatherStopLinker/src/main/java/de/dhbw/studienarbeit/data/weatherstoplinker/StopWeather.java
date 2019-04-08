e65f30d508852e58af28a51d0df444093509b7d6
ricokritzer <rico@kritzer.net>
2019-04-08 19:19:46 +0200

    Linker anhand geplanter Abfahrt: Vorteil: Ausfälle zählbar

--- Studienarbeit/DataWeatherStopLinker/src/main/java/de/dhbw/studienarbeit/data/weatherstoplinker/StopWeather.java
+++ Studienarbeit/DataWeatherStopLinker/src/main/java/de/dhbw/studienarbeit/data/weatherstoplinker/StopWeather.java
@@ -25,8 +25,8 @@ public class StopWeather implements Saveable
 		return "INSERT INTO StopWeather (stopID, weatherId) SELECT Stop.stopID, max(Weather.id) FROM Stop, Station, Weather "
 				+ "WHERE Stop.stationID = Station.stationID AND Stop.stopID = ? AND Stop.stopID NOT IN (SELECT stopID FROM StopWeather) "
 				+ "AND Weather.lat = ROUND(Station.lat, 2) AND Weather.lon = ROUND(Station.lon, 2) "
-				+ "AND Weather.timeStamp < Stop.realtime "
-				+ "AND Weather.timeStamp > DATE_SUB(Stop.realtime, INTERVAL 60 MINUTE) "
+				+ "AND Weather.timeStamp < Stop.timeTabledTime "
+				+ "AND Weather.timeStamp > DATE_SUB(Stop.timeTabledTime, INTERVAL 60 MINUTE) "
 				+ "GROUP BY Stop.stopID;";
 	}
 
