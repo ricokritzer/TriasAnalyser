156003c8d350b40623fd57a5ae8c6b73efaca131
ricokritzer <rico@kritzer.net>
2019-04-09 13:03:53 +0200

    SQL angepasst: Möglichkeit zum Zählen von ausgefallenen Stops in Helper

--- Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/reader/data/weather/DelayWeatherDBHelper.java
+++ Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/reader/data/weather/DelayWeatherDBHelper.java
@@ -12,6 +12,16 @@ public class DelayWeatherDBHelper
 				.append("max(UNIX_TIMESTAMP(Stop.realTime) - UNIX_TIMESTAMP(Stop.timeTabledTime)) AS delay_max, ")
 				.append(what).append(" AS ").append(name).append(" FROM StopWeather, Stop, Weather ")
 				.append("WHERE Stop.stopID = StopWeather.stopID AND StopWeather.weatherId = Weather.id ")
-				.append("GROUP BY ").append(name).append(" ORDER BY ").append(name).append(";").toString();
+				.append("AND realTime IS NOT NULL").append("GROUP BY ").append(name).append(" ORDER BY ").append(name)
+				.append(";").toString();
+	}
+
+	public static String buildSQLCancelledStops(final String what, final String name)
+	{
+		return new StringBuilder().append("SELECT ").append("count(*) AS total, ").append(what).append(" AS ")
+				.append(name).append(" FROM StopWeather, Stop, Weather ")
+				.append("WHERE Stop.stopID = StopWeather.stopID AND StopWeather.weatherId = Weather.id ")
+				.append("AND realTime IS NULL").append("GROUP BY ").append(name).append(" ORDER BY ").append(name)
+				.append(";").toString();
 	}
 }
