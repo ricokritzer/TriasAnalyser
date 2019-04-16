5256184d19a93362b92d4f4ee2907963ffa29ba1
ricokritzer <rico@kritzer.net>
2019-04-16 11:52:04 +0200

    Refactoring: Entfernen veralteter Methoden

--- Studienarbeit/DataReader/src/main/java/de/dhbw/studienarbeit/data/reader/data/station/DelayStation.java
+++
@@ -1,9 +0,0 @@
-package de.dhbw.studienarbeit.data.reader.data.station;
-
-import java.io.IOException;
-import java.util.List;
-
-public interface DelayStation
-{
-	List<DelayStationData> getDelays() throws IOException;
-}
