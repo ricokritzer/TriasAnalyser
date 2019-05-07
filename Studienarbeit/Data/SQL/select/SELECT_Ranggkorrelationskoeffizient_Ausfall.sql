3a47218e860411d25b3f0e015fd6033ba9c0e3e3
ricokritzer <rico@kritzer.net>
2019-05-08 00:03:07 +0200

    SQL-Rangkorrelationskoeffizienten

--- Studienarbeit/Data/SQL/select/SELECT_Ranggkorrelationskoeffizient_Ausfall.sql
+++ Studienarbeit/Data/SQL/select/SELECT_Ranggkorrelationskoeffizient_Ausfall.sql
@@ -1,14 +1,25 @@
 CREATE TABLE Correlation (
         id int AUTO_INCREMENT PRIMARY KEY,
         value1 decimal(6,2),
+        sum decimal(8),
+        cancelled decimal(8),
         value2 decimal(8),
         rank1 decimal(10,2),
         rank2 decimal(10,2),
         differenceSquare double);
         
-INSERT INTO Correlation (value1, value2) SELECT humidity, (UNIX_TIMESTAMP(realtime) - UNIX_TIMESTAMP(timeTabledTime))
-        FROM StopWeather, Stop, Weather
-        WHERE StopWeather.weatherID = Weather.id AND StopWeather.stopID = Stop.stopID AND realtime IS NOT NULL;
+INSERT INTO Correlation (value1) SELECT DISTINCT wind FROM Weather;
+
+UPDATE Correlation SET sum = (SELECT count(*) FROM StopWeather, Stop, Weather
+        WHERE StopWeather.weatherID = Weather.id AND StopWeather.stopID = Stop.stopID AND value1 = wind);
+
+DELETE FROM Correlation WHERE sum = 0;
+
+UPDATE Correlation SET cancelled = (SELECT count(*) FROM StopWeather, Stop, Weather
+        WHERE StopWeather.weatherID = Weather.id AND StopWeather.stopID = Stop.stopID AND realtime IS NULL AND value1 = wind);
+UPDATE Correlation SET cancelled = 0 WHERE cancelled IS NULL;
+
+UPDATE Correlation SET value2 = cancelled/sum * 1000;
 
 CREATE TABLE V1(
         value1 decimal(6,2) PRIMARY KEY,
@@ -43,6 +54,8 @@ UPDATE V1 SET rank = startRank + (total -1)/2;
 UPDATE Correlation SET rank1 = (SELECT rank FROM V1 WHERE V1.value1 = Correlation.value1);
 DROP TABLE V1;
 
+SELECT * FROM Correlation;
+
 INSERT INTO V2 (value2, total) SELECT Correlation.value2, count(*) FROM Correlation GROUP BY Correlation.value2 ORDER BY Correlation.value2;
 UPDATE V2 SET startrank = 1 ORDER BY value2 LIMIT 1;
 
@@ -51,7 +64,7 @@ UPDATE V2 SET startRank = (SELECT 1+sum(V2Copy.total) FROM V2Copy WHERE V2Copy.v
 DROP TABLE V2Copy;
 
 UPDATE V2 SET rank = startRank + (total -1)/2;
-UPDATE Correlation SET rank2 = (SELECT rank FROM V2 WHERE V2.value2 = Correlation.value2);
+UPDATE Correlation SET rank2 = (SELECT rank FROM V2 WHERE V2.value2 = ROUND(Correlation.value2,4));
 DROP TABLE V2;
 
 UPDATE Correlation SET differenceSquare = (rank1-rank2)*(rank1-rank2);
