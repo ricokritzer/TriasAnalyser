6b36b5cdf38479474a5a3864fb6404ebd3e49413
Patrick <pat-sie@web.de>
2019-05-09 11:45:03 +0200

    Todos für Situation Tabelle angepasst

--- Studienarbeit/WebView/src/main/java/de/dhbw/studienarbeit/WebView/components/DelaySituationDiv.java
+++ Studienarbeit/WebView/src/main/java/de/dhbw/studienarbeit/WebView/components/DelaySituationDiv.java
@@ -8,6 +8,7 @@ import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.provider.DataProvider;
+import com.vaadin.flow.data.renderer.TextRenderer;
 
 import de.dhbw.studienarbeit.data.reader.data.situation.DelaySituationData;
 import de.dhbw.studienarbeit.web.data.Data;
@@ -32,11 +33,18 @@ public class DelaySituationDiv extends Div
 		layout.add(field);
 
 		grid.addColumn(db -> db.getSituationName()).setHeader("Verkehrssituation").setSortable(true);
-		grid.addColumn(db -> db.getCountWithSituation().toString()).setHeader("Anzahl mit").setSortable(true);
-		grid.addColumn(db -> db.getDelayWithSituation().toString()).setHeader("Durchschnittliche Verspätung mit").setSortable(true);
-		grid.addColumn(db -> db.getCountWithoutSituation().toString()).setHeader("Anzahl ohne").setSortable(true);
-		grid.addColumn(db -> db.getDelayWithoutSituation().toString()).setHeader("Durchschnittliche Verspätung ohne").setSortable(true);
+		grid.addColumn(db -> db.getCountWithSituation().toString() + "/" + db.getCountWithoutSituation().toString())
+				.setHeader("Anzahl (mit/ohne Situation)").setSortable(true).setComparator((o1, o2) -> Long
+						.compare(o1.getCountWithSituation().getValue(), o2.getCountWithSituation().getValue()));
+		grid.addColumn(db -> db.getDelayWithSituation().toString() + "/" + db.getDelayWithoutSituation().toString())
+				.setHeader("Durchschnittliche Verspätung (mit/ohne Situation)").setSortable(false);
+		grid.addColumn(db -> db.getDifference().toString()).setHeader("Verspätungsänderung").setSortable(true)
+				.setComparator(
+						(o1, o2) -> Double.compare(o1.getDifference().getValue(), o2.getDifference().getValue()));
 		
+		grid.setDetailsVisibleOnClick(true);
+		grid.setItemDetailsRenderer(new TextRenderer<DelaySituationData>(e -> e.getSituationName().toString()));
+
 		grid.setDataProvider(DataProvider.ofCollection(Data.getDelaysSituation()));
 
 		grid.setHeight("70vh");
