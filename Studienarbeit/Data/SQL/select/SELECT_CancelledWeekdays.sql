4ae704e57b86885e5b32c5b3320c6983b2656cfa
ricokritzer <rico@kritzer.net>
2019-05-14 09:57:19 +0200

    Fehlerbehebung: Namen der Wochentage fehlerhaft

--- Studienarbeit/Data/SQL/select/SELECT_CancelledWeekdays.sql
+++ Studienarbeit/Data/SQL/select/SELECT_CancelledWeekdays.sql
@@ -6,13 +6,13 @@ CREATE TABLE CancelledWeekdays(
         total decimal(8),
         percentage decimal(8,5));
         
-INSERT INTO CancelledWeekdays(id, name) VALUES (1, "Mo");
-INSERT INTO CancelledWeekdays(id, name) VALUES (2, "Di");
-INSERT INTO CancelledWeekdays(id, name) VALUES (3, "Mi");
-INSERT INTO CancelledWeekdays(id, name) VALUES (4, "Do");
-INSERT INTO CancelledWeekdays(id, name) VALUES (5, "Fr");
-INSERT INTO CancelledWeekdays(id, name) VALUES (6, "Sa");
-INSERT INTO CancelledWeekdays(id, name) VALUES (7, "So");
+INSERT INTO CancelledWeekdays(id, name) VALUES (1, "So");
+INSERT INTO CancelledWeekdays(id, name) VALUES (2, "Mo");
+INSERT INTO CancelledWeekdays(id, name) VALUES (3, "Di");
+INSERT INTO CancelledWeekdays(id, name) VALUES (4, "Mi");
+INSERT INTO CancelledWeekdays(id, name) VALUES (5, "Do");
+INSERT INTO CancelledWeekdays(id, name) VALUES (6, "Fr");
+INSERT INTO CancelledWeekdays(id, name) VALUES (7, "Sa");
 
 UPDATE CancelledWeekdays SET cancelled = (SELECT COUNT(*) FROM Stop WHERE CancelledWeekdays.id = DAYOFWEEK(Stop.timetabledTime) AND realtime IS NULL);
 UPDATE CancelledWeekdays SET total = (SELECT COUNT(*) FROM Stop WHERE CancelledWeekdays.id = DAYOFWEEK(Stop.timetabledTime));
